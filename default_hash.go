dea6437ce51a3f95f536334968a1837f3309e728
Tommy TIAN <xtianae@connect.ust.hk>
2022-08-27 23:15:04 +0800

    Remove useless error checks in default hash functions (#16)
    Signed-off-by: txaty <xtianae@connect.ust.hk>

--- default_hash.go
+++ default_hash.go
@@ -32,10 +32,7 @@ var sha256Digest = sha256.New()
 // It implements SHA256 hash function.
 func defaultHashFunc(data []byte) ([]byte, error) {
 	defer sha256Digest.Reset()
-	_, err := sha256Digest.Write(data)
-	if err != nil {
-		return nil, err
-	}
+	sha256Digest.Write(data)
 	return sha256Digest.Sum(nil), nil
 
 }
@@ -45,9 +42,6 @@ func defaultHashFunc(data []byte) ([]byte, error) {
 // When implementing hash functions for paralleled algorithms, please make sure it is concurrent safe.
 func defaultHashFuncParal(data []byte) ([]byte, error) {
 	digest := sha256.New()
-	_, err := digest.Write(data)
-	if err != nil {
-		return nil, err
-	}
+	digest.Write(data)
 	return digest.Sum(nil), nil
 }
