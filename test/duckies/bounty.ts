7143cd55bc8d3e6ef7a9ca93223d92b6b807a832
MoD <mod@tail.cat>
2023-02-14 13:53:15 +0700

    Adding Garden with a few tests

---
+++ test/duckies/bounty.ts
@@ -0,0 +1,74 @@
+import { utils } from 'ethers';
+
+import { randomBytes32 } from '../helpers/payload';
+
+import type { ParamType } from 'ethers/lib/utils';
+
+export interface Bounty {
+  amount: number;
+  tokenAddress: string;
+  beneficiary: string;
+  isPaidToReferrers: boolean;
+  referrer: string;
+  expire: number;
+  chainId: number;
+  bountyUID: string;
+}
+
+export const BountyTy = {
+  name: 'bounty',
+  type: 'tuple',
+  components: [
+    {
+      name: 'amount',
+      type: 'uint256',
+    },
+    {
+      name: 'tokenAddress',
+      type: 'address',
+    },
+    {
+      name: 'beneficiary',
+      type: 'address',
+    },
+    {
+      name: 'isPaidToReferrers',
+      type: 'bool',
+    },
+    {
+      name: 'referrer',
+      type: 'address',
+    },
+    {
+      name: 'expire',
+      type: 'uint64',
+    },
+    {
+      name: 'chainId',
+      type: 'uint32',
+    },
+    {
+      name: 'bountyUID',
+      type: 'bytes32',
+    },
+  ],
+} as ParamType;
+
+export function encodeBounty(bounty: Bounty): string {
+  return utils.defaultAbiCoder.encode([BountyTy], [bounty]);
+}
+
+export function setBountyField<BountyKey extends keyof Bounty>(
+  bounty: Bounty,
+  field: BountyKey,
+  value: Bounty[BountyKey],
+): Bounty {
+  return {
+    ...bounty,
+    [field]: value,
+  };
+}
+
+export function setRandomBountyUID(bounty: Bounty): Bounty {
+  return { ...bounty, bountyUID: randomBytes32() };
+}
