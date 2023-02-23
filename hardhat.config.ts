595f67e8044eed39e8c1dfad48c392ef31029f78
nksazonov <nsazonov@openware.com>
2023-02-23 12:41:36 +0200

    decrease optimizer runs amount

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 20_000,
+            runs: 200,
           },
           viaIR: true,
         },
