918add28581712bb31833edf80f11f861e853bd3
Sazonov Nikita <35502225+nksazonov@users.noreply.github.com>
2023-02-24 12:40:49 +0200

    feat: move payouts to bounty (#7)
    * add NFT contracts
    
    * decrease optimizer runs amount
    
    * remove halving, move payouts to bounty
    
    ---------
    
    Co-authored-by: nksazonov <nsazonov@openware.com>

--- contracts/duckies/Garden.sol
+++ contracts/duckies/Garden.sol
@@ -6,7 +6,6 @@ import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol';
 
 contract Garden is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
@@ -23,8 +22,6 @@ contract Garden is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
 
 	// Constants
 	uint8 public constant REFERRAL_MAX_DEPTH = 5;
-	uint256 internal _DUCKIES_SUPPLY_CAP;
-	uint8 internal constant _MAX_HALVING_STEP = 5;
 	uint8 internal constant _REFERRAL_PAYOUT_DIVIDER = 100;
 
 	// Bounty Message for signature verification
@@ -33,6 +30,7 @@ contract Garden is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
 		address tokenAddress;
 		address beneficiary; // beneficiary of bounty
 		bool isPaidToReferrers; // whether bounty is payed to referrers
+		uint8[REFERRAL_MAX_DEPTH] referrersPayouts; // what percentage of bounty will referrer of the level specified get
 		address referrer; // address of the parent
 		uint64 expire; // expiration time in seconds UTC
 		uint32 chainId;
@@ -42,10 +40,6 @@ contract Garden is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
 	// child => parent
 	// TODO: make internal
 	mapping(address => address) internal _referrerOf;
-
-	uint16[REFERRAL_MAX_DEPTH] internal _baseReferralPayouts;
-
-	ERC20CappedUpgradeable internal _duckies;
 
 	address internal _issuer;
 
@@ -66,17 +60,12 @@ contract Garden is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
 		_disableInitializers();
 	}
 
-	function initialize(address duckies) public initializer {
+	function initialize() public initializer {
 		__AccessControl_init();
 		__UUPSUpgradeable_init();
 
 		_grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
 		_grantRole(UPGRADER_ROLE, msg.sender);
-
-		_baseReferralPayouts = [500, 125, 80, 50, 20];
-
-		_duckies = ERC20CappedUpgradeable(duckies);
-		_DUCKIES_SUPPLY_CAP = _duckies.cap();
 	}
 
 	// -------- Issuer --------
@@ -121,52 +110,6 @@ contract Garden is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
 		}
 	}
 
-	// -------- Payouts --------
-
-	function getReferralPayouts() external view returns (uint16[REFERRAL_MAX_DEPTH] memory) {
-		return _getReferralPayouts();
-	}
-
-	function setBaseReferralPayouts(
-		uint8[REFERRAL_MAX_DEPTH] memory referralPayouts
-	) public onlyRole(DEFAULT_ADMIN_ROLE) {
-		_baseReferralPayouts = referralPayouts;
-	}
-
-	function _getReferralPayouts() internal view returns (uint16[REFERRAL_MAX_DEPTH] memory) {
-		uint16[REFERRAL_MAX_DEPTH] memory referralPayouts = _baseReferralPayouts;
-		uint8 halvingDivider = halving();
-
-		if (halvingDivider >= 2 ** _MAX_HALVING_STEP) {
-			// return constant literal to save gas. Array length depends on REFERRAL_MAX_DEPTH value.
-			return [uint16(5), 4, 3, 2, 1];
-		}
-
-		for (uint i = 0; i < referralPayouts.length; i++) {
-			referralPayouts[i] /= halvingDivider;
-		}
-		return referralPayouts;
-	}
-
-	// -------- Halving --------
-
-	function halving() public view returns (uint8) {
-		uint16 nextHalvingStep = 1;
-
-		while (
-			nextHalvingStep < _MAX_HALVING_STEP &&
-			_duckies.totalSupply() >= _halvingThreshold(nextHalvingStep)
-		) {
-			nextHalvingStep++;
-		}
-
-		return uint8(2 ** (nextHalvingStep - 1));
-	}
-
-	function _halvingThreshold(uint16 halvingNum) internal view returns (uint256) {
-		return _DUCKIES_SUPPLY_CAP - (_DUCKIES_SUPPLY_CAP / 2 ** halvingNum);
-	}
-
 	// -------- Bounties --------
 
 	function claimBounties(Bounty[] calldata bounties, bytes calldata signature) external {
@@ -186,11 +129,12 @@ contract Garden is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
 
 		_claimedBounties[bounty.bountyCodeHash] = true;
 
+		// check sufficient Garden token balance and pay beneficiary
 		ERC20Upgradeable bountyToken = ERC20Upgradeable(bounty.tokenAddress);
-
 		_requireSufficientContractBalance(bountyToken, bounty.amount);
 		bountyToken.transfer(bounty.beneficiary, bounty.amount);
 
+		// check for circular reference and register referrer
 		// provided beneficiary has a referrer
 		if (bounty.referrer != address(0)) {
 			if (bounty.referrer == msg.sender) revert InvalidBounty(bounty);
@@ -200,19 +144,19 @@ contract Garden is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
 			_registerReferrer(bounty.beneficiary, bounty.referrer);
 		}
 
-		if (bounty.isPaidToReferrers) {
-			uint16[REFERRAL_MAX_DEPTH] memory referralRates = _getReferralPayouts();
-			address currReferrer = _referrerOf[bounty.beneficiary];
+		// pay referrers
+		address currReferrer = _referrerOf[bounty.beneficiary];
 
-			for (uint8 i = 0; i < REFERRAL_MAX_DEPTH && currReferrer != address(0); i++) {
-				uint256 referralAmount = (bounty.amount * referralRates[i]) /
+		for (uint8 i = 0; i < REFERRAL_MAX_DEPTH && currReferrer != address(0); i++) {
+			if (bounty.referrersPayouts[i] != 0) {
+				uint256 referralAmount = (bounty.amount * bounty.referrersPayouts[i]) /
 					_REFERRAL_PAYOUT_DIVIDER;
 
 				_requireSufficientContractBalance(bountyToken, referralAmount);
 				bountyToken.transfer(currReferrer, referralAmount);
-
-				currReferrer = _referrerOf[currReferrer];
 			}
+
+			currReferrer = _referrerOf[currReferrer];
 		}
 
 		emit BountyClaimed(
