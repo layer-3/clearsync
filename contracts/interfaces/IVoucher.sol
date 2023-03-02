439dc88a89ae704e27b16fb0d6fafd27fa0257f6
nksazonov <nsazonov@openware.com>
2023-03-02 15:51:20 +0200

    extract some types from voucher, add functs

--- contracts/interfaces/IVoucher.sol
+++ contracts/interfaces/IVoucher.sol
@@ -3,52 +3,43 @@ pragma solidity 0.8.17;
 
 /**
  * @notice Interface describing Voucher for redeeming game items
- * 
+ *
  * @dev The Voucher type must have a strict implementation on backend
- * 
+ *
  * A Voucher is a document signed from the server IssuerKey and allows the execution
  * of actions on the game generally for creating game items, such as Booster Packs, Meld or reward tokens
  *
  */
 interface IVoucher {
+	/**
+	 * @dev Build and encode the Voucher from server side
+	 *
+	 * Voucher structure will be valid only in chainId until expire timestamp
+	 * the beneficiary MUST be the same as the user redeeming the Voucher.
+	 *
+	 */
+	struct Voucher {
+		address target; // contract address which the voucher is meant for
+		uint8 action; // voucher type defined by the implementation
+		address beneficiary; // beneficiary account which voucher will redeem to
+		address referrer; // address of the parent
+		uint64 expire; // expiration time in seconds UTC
+		uint32 chainId; // chain id of the voucher
+		bytes32 voucherCodeHash; // hash of voucherCode
+		bytes encodedParams; // voucher type specific encoded params
+	}
 
-    enum VoucherType {
-        Reward,
-        MintPacks,
-        MeldFlock
-    }
+	/**
+	 * @notice Use vouchers that were issued and signed by the Back-end to receive game items.
+	 * @param vouchers Vouchers issued by the Back-end.
+	 * @param signature Vouchers signed by the Back-end.
+	 */
+	function useVouchers(Voucher[] calldata vouchers, bytes calldata signature) external;
 
-    struct RewardParams {
-        address token;          // address of the ERC20 token to payout
-        uint256 amount;         // amount of token to be pay
-        uint8[5] commissions;   // what percentage of bounty will referrer of the level specified get
-    }
-
-    struct MintPacksParams {
-        address token;      // address of ERC721 token to mint
-        uint8 collection;   // collection index
-        uint8 size;         // card booster pack size
-    }
-
-    struct MeldFlockParams {
-        uint256[5] flockTokenIds; // token Ids to meld
-    }
-
-    /**
-     * @dev Build and encode the Voucher from server side
-     *
-     * Voucher structure will be valid only in chainId until expire timestamp
-     * the beneficiary MUST be the same as the user redeeming the Voucher.
-     * 
-     */
-    struct Voucher {
-        VoucherType action;             // voucher type
-        address     target;             // contract address which the voucher was meant for
-        address     beneficiary;        // beneficiary account which voucher will redeem to
-        address     referrer;           // address of the parent
-        uint64      expire;             // expiration time in seconds UTC
-        uint32      chainId;            // chain id of the voucher
-        bytes32     voucherCodeHash;    // hash of voucherCode
-        bytes       encodedParams;      // voucher type specific encoded params
-    }
+	/**
+	 * @notice Use the voucher that was signed by the Back-end to receive game items.
+	 * @param voucher Voucher issued by the Back-end.
+	 * @param signature Voucher signed by the Back-end.
+	 */
+	function useVoucher(Voucher calldata voucher, bytes calldata signature) external;
 }
