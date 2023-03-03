eb12ce67bbeaee961fcf0b14f2fb3bebfe0df25d
nksazonov <nsazonov@openware.com>
2023-03-03 19:34:20 +0200

    fix deployCanary cap decimals

--- scripts/deployCanary.ts
+++ scripts/deployCanary.ts
@@ -2,7 +2,7 @@ import { ethers } from 'hardhat';
 
 async function main(): Promise<void> {
   const TokenFactory = await ethers.getContractFactory('Token');
-  const Token = await TokenFactory.deploy('Canary', 'CANARY', 1_000_000_000);
+  const Token = await TokenFactory.deploy('Canary', 'CANARY', 1_000_000_000n * 10n ** 8n);
 
   await Token.deployed();
 
