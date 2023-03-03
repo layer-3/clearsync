ba9f06b6cfd7afe3422d54e3b20becd5d5522287
nksazonov <nsazonov@openware.com>
2023-03-03 16:19:56 +0200

    update solc to 0.8.19

--- contracts/duckies/TreasureVault.sol
+++ contracts/duckies/TreasureVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.19;
 
 import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,7 +19,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.18',
+        version: '0.8.19',
         settings: {
           optimizer: {
             enabled: true,
