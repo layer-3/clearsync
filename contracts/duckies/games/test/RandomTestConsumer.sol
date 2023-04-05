34eeacf9c194ab7a9f1be0abf7076126d5951893
nksazonov <nsazonov@openware.com>
2023-04-05 12:27:37 +0300

    make random not upgradeable

--- contracts/duckies/games/test/RandomTestConsumer.sol
+++ contracts/duckies/games/test/RandomTestConsumer.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import '../RandomUpgradeable.sol';
+import '../Random.sol';
 
-contract RandomTestConsumer is RandomUpgradeable {
+contract RandomTestConsumer is Random {
 	event NumberGenerated(uint256 number);
 
 	function randomMaxNumber(uint256 max) external UseRandom {
