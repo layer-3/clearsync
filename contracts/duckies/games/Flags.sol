0ebfa480519ddeb0c3436aa1551a6de3b5f7eaaf
nksazonov <nsazonov@openware.com>
2023-04-11 12:51:14 +0300

    add setFlag function

--- contracts/duckies/games/Flags.sol
+++ contracts/duckies/games/Flags.sol
@@ -4,12 +4,20 @@ pragma solidity 0.8.18;
 library Flags {
 	uint8 public constant IS_TRANSFERABLE_FLAG = 0;
 
-	function setFlag(uint8 self, uint8 flag) internal pure returns (uint8) {
+	function raiseFlag(uint8 self, uint8 flag) internal pure returns (uint8) {
 		return uint8(self | (1 << flag));
 	}
 
 	function clearFlag(uint8 self, uint8 flag) internal pure returns (uint8) {
 		return uint8(self & ~(1 << flag));
+	}
+
+	function setFlag(uint8 self, uint8 flag, bool value) internal pure returns (uint8) {
+		if (value) {
+			return raiseFlag(self, flag);
+		} else {
+			return clearFlag(self, flag);
+		}
 	}
 
 	function getFlag(uint8 flags, uint8 flag) internal pure returns (bool) {
