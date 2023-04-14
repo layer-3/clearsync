b2b836cb4ea6e6dbfe07160c0e00c316697cbdbb
nksazonov <nsazonov@openware.com>
2023-04-14 13:49:19 +0300

    extract calcUniqIdGenerationParams and add tests

--- test/duckies/games/DuckyFamily/DFV1_helpers.test.ts
+++ test/duckies/games/DuckyFamily/DFV1_helpers.test.ts
@@ -42,4 +42,29 @@ describe('DuckyGameV1 helpers', () => {
       ),
     ).to.equal(5 + 6 + 7 + 8 + 9 + 11 + 12 + 14); // 001111101101
   });
+
+  describe.only('_calcUniqIdGenerationParams', () => {
+    const maxUniqId = 59;
+
+    it('mythic id range overlaps with left dispersion border', async () => {
+      expect(await Game.calcUniqIdGenerationParams(0, maxUniqId)).to.deep.equal([0, 5]);
+      expect(await Game.calcUniqIdGenerationParams(2, maxUniqId)).to.deep.equal([0, 7]);
+      expect(await Game.calcUniqIdGenerationParams(4, maxUniqId)).to.deep.equal([0, 9]);
+      expect(await Game.calcUniqIdGenerationParams(5, maxUniqId)).to.deep.equal([0, 10]);
+    });
+
+    it('mythic id range overlaps with right dispersion border', async () => {
+      expect(await Game.calcUniqIdGenerationParams(55, maxUniqId)).to.deep.equal([50, 10]);
+      expect(await Game.calcUniqIdGenerationParams(56, maxUniqId)).to.deep.equal([51, 9]);
+      expect(await Game.calcUniqIdGenerationParams(57, maxUniqId)).to.deep.equal([52, 8]);
+      expect(await Game.calcUniqIdGenerationParams(59, maxUniqId)).to.deep.equal([54, 6]);
+    });
+
+    it('mythic id range does not overlap with dispersion borders', async () => {
+      expect(await Game.calcUniqIdGenerationParams(6, maxUniqId)).to.deep.equal([1, 10]);
+      expect(await Game.calcUniqIdGenerationParams(7, maxUniqId)).to.deep.equal([2, 10]);
+      expect(await Game.calcUniqIdGenerationParams(8, maxUniqId)).to.deep.equal([3, 10]);
+      expect(await Game.calcUniqIdGenerationParams(54, maxUniqId)).to.deep.equal([49, 10]);
+    });
+  });
 });
