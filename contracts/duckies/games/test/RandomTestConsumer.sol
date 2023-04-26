c52572f1c7d364bb2d35b16dfa9c0469b594becc
nksazonov <nsazonov@openware.com>
2023-04-26 12:10:32 +0300

    fix random tests

--- contracts/duckies/games/test/RandomTestConsumer.sol
+++ contracts/duckies/games/test/RandomTestConsumer.sol
@@ -4,15 +4,24 @@ pragma solidity 0.8.18;
 import '../Random.sol';
 
 contract RandomTestConsumer is Random {
-	event NumberGenerated(uint256 number);
+	event SeedGenerated(bytes32 seed);
 
-	function random(uint256 max, bytes3 seed) external useRandom {
-		uint256 num = _random(max, seed);
-		emit NumberGenerated(num);
+	function randomSeed() external {
+		emit SeedGenerated(_randomSeed());
 	}
 
-	function randomWeightedNumber(uint32[] memory weights, bytes3 seed) external useRandom {
-		uint256 num = _randomWeightedNumber(weights, seed);
-		emit NumberGenerated(num);
+	function rotateSeedChunk(bytes32 seed) external pure returns (bytes3, bytes32) {
+		return _rotateSeedChunk(seed);
+	}
+
+	function random(uint256 max, bytes3 seed) external view returns (uint256) {
+		return _random(max, seed);
+	}
+
+	function randomWeightedNumber(
+		uint32[] memory weights,
+		bytes3 seed
+	) external view returns (uint256) {
+		return _randomWeightedNumber(weights, seed);
 	}
 }
