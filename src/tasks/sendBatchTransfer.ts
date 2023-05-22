fffc16b8e688432591ce52a837207a5d79e5d2c2
Sazonov Nikita <35502225+nksazonov@users.noreply.github.com>
2023-05-22 13:38:30 +0300

    fix: hardhat artifacts circular dependency (#72)
    * fix circular artifact dep
    
    * change eslint promise rule
    
    ---------
    
    Co-authored-by: nksazonov <nsazonov@openware.com>

--- src/tasks/sendBatchTransfer.ts
+++ src/tasks/sendBatchTransfer.ts
@@ -1,12 +1,13 @@
-import { task, types } from 'hardhat/config';
-import { BatchTransfer__factory, ERC20__factory } from '../../typechain-types';
+import fs from 'node:fs';
+import readline from 'node:readline';
 
-import fs from 'fs';
-import readline from 'readline';
+import { task, types } from 'hardhat/config';
+
+import type { BatchTransfer, ERC20 } from '../../typechain-types';
 
 const MAX_BATCH_SIZE = 500;
 const DEFAULT_INTERVAL = 10; // minutes
-const addressRegex = /^0x[0-9a-fA-F]{40}$/;
+const addressRegex = /^0x[\dA-Fa-f]{40}$/;
 
 interface TaskArgs {
   addressesPath: string;
@@ -26,9 +27,19 @@ task('sendBatchTransfer', 'Send batch transfer')
   .addParam('amount', 'The amount to send')
   .addOptionalParam('minBatchSize', 'The minimum batch size', undefined, types.int)
   .addOptionalParam('maxBatchSize', 'The maximum batch size', undefined, types.int)
-  .addOptionalParam('minInterval', 'The minimum interval between batches (minutes)', undefined, types.int)
-  .addOptionalParam('maxInterval', 'The maximum interval between batches (minutes)', undefined, types.int)
-  .setAction(async (taskArgs: TaskArgs, hre) => {
+  .addOptionalParam(
+    'minInterval',
+    'The minimum interval between batches (minutes)',
+    undefined,
+    types.int,
+  )
+  .addOptionalParam(
+    'maxInterval',
+    'The maximum interval between batches (minutes)',
+    undefined,
+    types.int,
+  )
+  .setAction(async (taskArgs: TaskArgs, { ethers }) => {
     const { addressesPath, tokenAddress, batcherAddress, amount } = taskArgs;
     const minBatchSize = taskArgs.minBatchSize ?? MAX_BATCH_SIZE;
     const maxBatchSize = taskArgs.maxBatchSize ?? MAX_BATCH_SIZE;
@@ -36,27 +47,37 @@ task('sendBatchTransfer', 'Send batch transfer')
     const maxInterval = taskArgs.maxInterval ?? DEFAULT_INTERVAL;
 
     if (minBatchSize > maxBatchSize) {
-      throw new Error(`minBatchSize must be less than or equal to maxBatchSize: ${minBatchSize} > ${maxBatchSize}`);
+      throw new Error(
+        `minBatchSize must be less than or equal to maxBatchSize: ${minBatchSize} > ${maxBatchSize}`,
+      );
     } else if (minInterval > maxInterval) {
-      throw new Error(`minInterval must be less than or equal to maxInterval: ${minInterval} > ${maxInterval}`);
+      throw new Error(
+        `minInterval must be less than or equal to maxInterval: ${minInterval} > ${maxInterval}`,
+      );
     } else if (maxBatchSize > MAX_BATCH_SIZE) {
       throw new Error(`maxBatchSize must be less than or equal to ${MAX_BATCH_SIZE}`);
     }
 
-    const [sender] = await hre.ethers.getSigners();
-  
+    const [sender] = await ethers.getSigners();
+
     console.log('Sending airdrops from address:', sender.address);
     const balanceBN = await sender.getBalance();
     console.log('Native balance:', balanceBN.toString());
-  
+
     const addresses = await parseAddressesFile(addressesPath);
     const quantity = addresses.length;
     console.log(`Processed file with ${quantity} addresses`);
-  
-    const token = ERC20__factory.connect(tokenAddress, sender);
-    const decimals = await token.decimals();
+
+    const Token = (await ethers.getContractAt('ERC20', tokenAddress, sender)) as ERC20;
+    const decimals = await Token.decimals();
     const amountFormatted = amount * 10 ** decimals;
     console.log(`Sending ${amountFormatted} tokens to each address`);
+
+    const BatchTransfer = (await ethers.getContractAt(
+      'BatchTransfer',
+      batcherAddress,
+      sender,
+    )) as BatchTransfer;
 
     let i = 0;
     while (i < addresses.length) {
@@ -64,39 +85,37 @@ task('sendBatchTransfer', 'Send batch transfer')
       if (i + batchSize > addresses.length) {
         batchSize = addresses.length - i;
       }
-      console.log(`Sending batch of ${batchSize} addresses...`)
+      console.log(`Sending batch of ${batchSize} addresses...`);
 
       const batchAddresses = addresses.slice(i, i + batchSize);
-  
-      const contract = BatchTransfer__factory.connect(batcherAddress, sender);
-      const tx = await contract.batchTransfer(tokenAddress, batchAddresses, amountFormatted);
-      console.log(`${i+1}. Transaction hash: ${tx.hash}`);
-  
+      const tx = await BatchTransfer.batchTransfer(tokenAddress, batchAddresses, amountFormatted);
+      console.log(`${i + 1}. Transaction hash: ${tx.hash}`);
+
       const interval = Math.floor(Math.random() * (maxInterval - minInterval + 1) + minInterval);
       console.log(`Waiting for ${interval} minutes...`);
-      await new Promise(resolve => setTimeout(resolve, interval * 60 * 1000));
-  
+      await new Promise((resolve) => setTimeout(resolve, interval * 60 * 1000));
+
       i += batchSize;
     }
   });
 
 async function parseAddressesFile(path: string): Promise<string[]> {
-  return new Promise((resolve, _) => {
-    let addresses: string[] = [];
+  return new Promise((resolve, __) => {
+    const addresses: string[] = [];
 
     const reader = fs.createReadStream(path);
     const rl = readline.createInterface({
       input: reader,
-      crlfDelay: Infinity
+      crlfDelay: Number.POSITIVE_INFINITY,
     });
 
     rl.on('line', (address) => {
       if (!addressRegex.test(address)) {
         console.log(`Invalid address: ${address}`);
-        return
+        return;
       }
 
-      if (address === '0x0000000000000000000000000000000000000000') return
+      if (address === '0x0000000000000000000000000000000000000000') return;
 
       addresses.push(address);
     }).on('close', () => {
