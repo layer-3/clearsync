2c447481a124a8b7dc03fc80ec79f88d88790faf
nksazonov <nsazonov@openware.com>
2023-05-23 13:25:12 +0300

    strictify solidity 0.8.18

--- contracts/duckies/VestingVault.sol
+++ contracts/duckies/VestingVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
