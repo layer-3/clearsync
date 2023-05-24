d18fcf895b0ca9b4e181216113f96182a2d30c29
nksazonov <nsazonov@openware.com>
2023-05-24 13:49:40 +0300

    add multiple schedules claim tests

--- test/duckies/VestingVault.test.ts
+++ test/duckies/VestingVault.test.ts
@@ -11,8 +11,8 @@ import type { TestERC20, VestingVault } from '../../typechain-types';
 const TOKEN_CAP = 100_000_000_000_000;
 const TOKEN_DECIMALS = 8;
 const NOW = Math.floor(Date.now() / 1000);
-const IN_FUTURE = NOW + 60 * 10;
-const IN_PAST = NOW - 60 * 10;
+const TIME_DIFF = 60 * 10; // 10 minutes
+const VESTING_1_START = NOW + TIME_DIFF;
 
 describe('Vesting', function () {
   let Owner: SignerWithAddress, Beneficiary: SignerWithAddress, Someone: SignerWithAddress;
@@ -60,7 +60,7 @@ describe('Vesting', function () {
         VestingAsOwner.addSchedule(
           BeneficiaryAddress,
           ethers.utils.parseUnits('100', TOKEN_DECIMALS),
-          IN_FUTURE,
+          VESTING_1_START,
           100,
         ),
       )
@@ -68,7 +68,7 @@ describe('Vesting', function () {
         .withArgs(
           BeneficiaryAddress,
           ethers.utils.parseUnits('100', TOKEN_DECIMALS),
-          IN_FUTURE,
+          VESTING_1_START,
           100,
         );
     });
@@ -78,7 +78,7 @@ describe('Vesting', function () {
         VestingAsSomeone.addSchedule(
           BeneficiaryAddress,
           ethers.utils.parseUnits('100', TOKEN_DECIMALS),
-          IN_FUTURE,
+          VESTING_1_START,
           100,
         ),
       ).to.be.revertedWith('Ownable: caller is not the Owner');
@@ -86,7 +86,7 @@ describe('Vesting', function () {
 
     it('revert when adding schedule with zero amount', async function () {
       await expect(
-        VestingAsOwner.addSchedule(BeneficiaryAddress, 0, IN_FUTURE, 100),
+        VestingAsOwner.addSchedule(BeneficiaryAddress, 0, VESTING_1_START, 100),
       ).to.be.revertedWithCustomError(Vesting, 'InvalidSchedule');
     });
 
@@ -106,7 +106,7 @@ describe('Vesting', function () {
         VestingAsOwner.addSchedule(
           BeneficiaryAddress,
           ethers.utils.parseUnits('100', TOKEN_DECIMALS),
-          IN_PAST,
+          NOW - 42,
           100,
         ),
       ).to.be.revertedWithCustomError(Vesting, 'InvalidSchedule');
@@ -118,7 +118,7 @@ describe('Vesting', function () {
       await VestingAsOwner.addSchedule(
         BeneficiaryAddress,
         ethers.utils.parseUnits('100', TOKEN_DECIMALS),
-        IN_FUTURE,
+        VESTING_1_START,
         100,
       );
       await expect(Vesting.connect(Owner).deleteSchedule(BeneficiaryAddress, 0))
@@ -130,7 +130,7 @@ describe('Vesting', function () {
       await VestingAsOwner.addSchedule(
         BeneficiaryAddress,
         ethers.utils.parseUnits('100', TOKEN_DECIMALS),
-        IN_FUTURE,
+        VESTING_1_START,
         100,
       );
       await expect(VestingAsSomeone.deleteSchedule(BeneficiaryAddress, 0)).to.be.revertedWith(
@@ -146,9 +146,9 @@ describe('Vesting', function () {
     });
   });
 
-  describe.only('claim', () => {
+  describe('claim', () => {
     const vestingAmount = ethers.utils.parseUnits('100', TOKEN_DECIMALS);
-    const vestingPeriod = 60 * 60 * 24 * 10;
+    const vestingDuration = 60 * 60 * 24 * 10; // 10 days
 
     let snapshot: SnapshotRestorer;
 
@@ -165,15 +165,15 @@ describe('Vesting', function () {
         await VestingAsOwner.addSchedule(
           BeneficiaryAddress,
           vestingAmount,
-          IN_FUTURE,
-          vestingPeriod,
+          VESTING_1_START,
+          vestingDuration,
         );
       });
 
       it('claim all tokens after vesting period ends', async function () {
         expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(0);
 
-        await setNextBlockTimestamp(IN_FUTURE + vestingPeriod);
+        await setNextBlockTimestamp(VESTING_1_START + vestingDuration);
         await VestingAsBeneficiary.claim();
 
         expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(vestingAmount);
@@ -181,29 +181,29 @@ describe('Vesting', function () {
 
       it('claim part of the tokens before vesting period ends', async function () {
         const timeDiff = 60 * 60 * 24 * 5;
-        await setNextBlockTimestamp(IN_FUTURE + timeDiff);
+        await setNextBlockTimestamp(VESTING_1_START + timeDiff);
         await VestingAsBeneficiary.claim();
 
         expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(
-          vestingAmount.mul(timeDiff).div(vestingPeriod),
+          vestingAmount.mul(timeDiff).div(vestingDuration),
         );
       });
 
       it('claim consequently', async function () {
         const timeDiff1 = 60 * 60 * 24 * 2;
-        await setNextBlockTimestamp(IN_FUTURE + timeDiff1);
+        await setNextBlockTimestamp(VESTING_1_START + timeDiff1);
         await VestingAsBeneficiary.claim();
 
         expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(
-          vestingAmount.mul(timeDiff1).div(vestingPeriod),
+          vestingAmount.mul(timeDiff1).div(vestingDuration),
         );
 
         const timeDiff2 = 60 * 60 * 24 * 4;
-        await setNextBlockTimestamp(IN_FUTURE + timeDiff2);
+        await setNextBlockTimestamp(VESTING_1_START + timeDiff2);
         await VestingAsBeneficiary.claim();
 
         expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(
-          vestingAmount.mul(timeDiff2).div(vestingPeriod),
+          vestingAmount.mul(timeDiff2).div(vestingDuration),
         );
       });
 
@@ -220,7 +220,7 @@ describe('Vesting', function () {
       });
 
       it('revert when no tokens to claim', async function () {
-        await setNextBlockTimestamp(IN_FUTURE + vestingPeriod);
+        await setNextBlockTimestamp(VESTING_1_START + vestingDuration);
         await VestingAsBeneficiary.claim();
 
         await expect(VestingAsBeneficiary.claim())
@@ -229,8 +229,90 @@ describe('Vesting', function () {
       });
     });
 
-    describe('multiple schedules', () => {
-      it.skip('claim from multiple schedules');
+    describe.only('multiple schedules', () => {
+      const VESTING_AMOUNT_2 = vestingAmount.mul(2);
+      const VESTING_2_START_SHIFT = vestingDuration / 2;
+      const VESTING_2_START = VESTING_1_START + VESTING_2_START_SHIFT;
+
+      // starts and vesting periods are the same
+      beforeEach(async function () {
+        await VestingAsOwner.addSchedule(
+          BeneficiaryAddress,
+          vestingAmount,
+          VESTING_1_START,
+          vestingDuration,
+        );
+        await VestingAsOwner.addSchedule(
+          BeneficiaryAddress,
+          VESTING_AMOUNT_2,
+          VESTING_2_START,
+          vestingDuration,
+        );
+      });
+
+      it('claim all tokens after multiple schedules end', async () => {
+        // after both schedules end
+        expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(0);
+
+        await setNextBlockTimestamp(VESTING_2_START + vestingDuration);
+        await VestingAsBeneficiary.claim();
+
+        expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(
+          vestingAmount.add(VESTING_AMOUNT_2),
+        );
+      });
+
+      it('claim part of the tokens before any schedule ends', async () => {
+        // when both schedules are active: 1st schedule is 75% done, 2nd schedule is 25% done
+        const timeDiff = VESTING_2_START_SHIFT * 1.5;
+        await setNextBlockTimestamp(VESTING_1_START + timeDiff);
+        await VestingAsBeneficiary.claim();
+
+        const expectedBalance = vestingAmount
+          .mul(timeDiff)
+          .div(vestingDuration)
+          .add(
+            VESTING_AMOUNT_2.mul(VESTING_1_START + timeDiff - VESTING_2_START).div(vestingDuration),
+          );
+
+        expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(expectedBalance);
+      });
+
+      it('claim consequently', async () => {
+        const timeDiff1 = VESTING_2_START_SHIFT - 100;
+        await setNextBlockTimestamp(VESTING_1_START + timeDiff1);
+        await VestingAsBeneficiary.claim();
+
+        expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(
+          vestingAmount.mul(timeDiff1).div(vestingDuration),
+        );
+
+        const timeDiff2 = VESTING_2_START_SHIFT + 4200;
+        await setNextBlockTimestamp(VESTING_1_START + timeDiff2);
+        await VestingAsBeneficiary.claim();
+
+        expect(await ERC20.balanceOf(BeneficiaryAddress)).to.equal(
+          vestingAmount
+            .mul(timeDiff2)
+            .div(vestingDuration)
+            .add(VESTING_AMOUNT_2.mul(4200).div(vestingDuration)),
+        );
+      });
+
+      it('revert when neither vesting has not started', async function () {
+        await expect(VestingAsBeneficiary.claim())
+          .to.be.revertedWithCustomError(Vesting, 'UnableToClaim')
+          .withArgs(BeneficiaryAddress);
+      });
+
+      it('revert when no tokens to claim', async function () {
+        await setNextBlockTimestamp(VESTING_2_START + vestingDuration);
+        await VestingAsBeneficiary.claim();
+
+        await expect(VestingAsBeneficiary.claim())
+          .to.be.revertedWithCustomError(Vesting, 'UnableToClaim')
+          .withArgs(BeneficiaryAddress);
+      });
     });
   });
 });
