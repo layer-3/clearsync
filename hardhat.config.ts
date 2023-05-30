f70ec982209a28223232717783e13f43bf035dfa
nksazonov <nsazonov@openware.com>
2023-05-30 16:13:28 +0300

    merge game-related tasks into one file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,10 +9,8 @@ import 'solidity-docgen';
 import './src/tasks/accounts';
 import './src/tasks/activate';
 import './src/tasks/forceImport';
-import './src/tasks/setupNFTs';
 import './src/tasks/sendBatchTransfer';
-import './src/tasks/setMintPrice';
-import './src/tasks/setMeldPrices';
+import './src/tasks/ducky-game';
 
 let accounts;
 
