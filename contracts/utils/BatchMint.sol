8bab4cf8ea43c5b406a99b24d39dd8af181fb1a9
nksazonov <nsazonov@openware.com>
2023-06-14 10:23:58 +0300

    add BatchMint contract

---
+++ contracts/utils/BatchMint.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.18;
+
+import '../duckies/ducklings/DucklingsV1.sol';
+
+contract BatchMint {
+	DucklingsV1 public ducklings;
+
+	constructor(address _ducklings) {
+		ducklings = DucklingsV1(_ducklings);
+	}
+
+	function mintToAddresses(uint256[] calldata genomes, address[] calldata addresses) external {
+		require(genomes.length > 0 && genomes.length == addresses.length, 'Invalid input lengths');
+
+		for (uint256 i = 0; i < genomes.length; i++) {
+			ducklings.mintTo(addresses[i], genomes[i]);
+		}
+	}
+}
