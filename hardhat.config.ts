a2bdea0485f1fe718a2ae49efb640f63450f9a6e
nksazonov <nsazonov@openware.com>
2023-06-22 16:27:19 +0300

    install hardhat-change-network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,7 @@ import '@nomiclabs/hardhat-ethers';
 import '@openzeppelin/hardhat-upgrades';
 import type { HardhatUserConfig } from 'hardhat/config';
 import 'solidity-docgen';
+import 'hardhat-change-network';
 
 import './src/tasks/accounts';
 import './src/tasks/activate';
