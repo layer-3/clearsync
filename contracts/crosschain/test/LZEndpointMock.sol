2b284926c1c23633660ad0306c14efb5719fc798
nksazonov <nsazonov@openware.com>
2023-07-11 14:52:07 +0300

    add external LZEndpointMock contract

---
+++ contracts/crosschain/test/LZEndpointMock.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.18;
+
+// import contract for Hardhat to compile to artifacts
+import '@layerzerolabs/solidity-examples/contracts/mocks/LZEndpointMock.sol';
