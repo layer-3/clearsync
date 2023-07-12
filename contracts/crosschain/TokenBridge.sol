6c88efc5f53035ff565a65149b82c2827cbd61ad
nksazonov <nsazonov@openware.com>
2023-07-12 12:10:00 +0300

    add BRIDGER_ROLE requirement

--- contracts/crosschain/TokenBridge.sol
+++ contracts/crosschain/TokenBridge.sol
@@ -2,12 +2,15 @@
 pragma solidity 0.8.18;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@layerzerolabs/solidity-examples/contracts/lzApp/NonblockingLzApp.sol';
 
 import '../interfaces/ITokenBridge.sol';
 import '../interfaces/IERC20MintableBurnable.sol';
 
-contract TokenBridge is ITokenBridge, NonblockingLzApp {
+contract TokenBridge is ITokenBridge, NonblockingLzApp, AccessControl {
+	bytes32 public constant BRIDGER_ROLE = keccak256('BRIDGER_ROLE');
+
 	IERC20MintableBurnable public tokenContract;
 	bool public immutable isRootBridge;
 
@@ -16,6 +19,9 @@ contract TokenBridge is ITokenBridge, NonblockingLzApp {
 		address tokenAddress,
 		bool isRootBridge_
 	) NonblockingLzApp(endpoint) {
+		grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+		grantRole(BRIDGER_ROLE, msg.sender);
+
 		tokenContract = IERC20MintableBurnable(tokenAddress);
 		isRootBridge = isRootBridge_;
 	}
@@ -26,7 +32,14 @@ contract TokenBridge is ITokenBridge, NonblockingLzApp {
 		uint64 _nonce,
 		bytes memory _payload
 	) internal override {
-		(address receiver, uint256 amount) = abi.decode(_payload, (address, uint256));
+		(address sender, address receiver, uint256 amount) = abi.decode(
+			_payload,
+			(address, address, uint256)
+		);
+
+		if (!hasRole(BRIDGER_ROLE, sender)) {
+			revert BridgingUnauthorized(sender);
+		}
 
 		if (isRootBridge) {
 			// NOTE: Bridge should have enough tokens as the only ability for token to appear on other chains is to be transferred to the bridge
@@ -62,7 +75,7 @@ contract TokenBridge is ITokenBridge, NonblockingLzApp {
 		uint256 amount,
 		address zroPaymentAddress,
 		bytes calldata adapterParams
-	) external payable {
+	) external payable onlyRole(BRIDGER_ROLE) {
 		if (isRootBridge) {
 			tokenContract.transferFrom(msg.sender, address(this), amount);
 		} else {
@@ -71,7 +84,7 @@ contract TokenBridge is ITokenBridge, NonblockingLzApp {
 
 		_lzSend(
 			chainId, // chainId
-			abi.encode(receiver, amount), // payload
+			abi.encode(msg.sender, receiver, amount), // payload
 			payable(msg.sender), // refundAddress
 			zroPaymentAddress, // zroPaymentAddress
 			adapterParams, // adapterParams
