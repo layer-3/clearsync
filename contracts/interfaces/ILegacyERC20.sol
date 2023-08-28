0d9087ee4f80ad33c4f4a42b8b6e07adf4a3bec0
nksazonov <nsazonov@openware.com>
2023-08-28 17:23:37 +0300

    remove unused files

--- contracts/interfaces/ILegacyERC20.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-interface ILegacyERC20 {
-    function transfer(address to, uint256 amount) external;
-
-    function transferFrom(address from, address to, uint256 amount) external;
-}
