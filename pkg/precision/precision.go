5e9d4c20f7e2bde2b63458e2c98a03437b7b3afd
Max Pushkarov <mpushkarov@openware.center>
2023-12-06 15:30:18 +0200

    refactor: use math calculations instead of string parsing

--- pkg/precision/precision.go
+++ pkg/precision/precision.go
@@ -1,186 +1,34 @@
 package precision
 
 import (
-	"strings"
-	"unsafe"
+	"math"
 
 	"github.com/shopspring/decimal"
 )
 
-type params struct {
-	digitsBefore      int
-	dot               int
-	leadingZeros      int
-	roundTo           int
-	significantDigits int
-	maxDecimals       int
-}
-
-func ToSignificant(x decimal.Decimal, significantDigits, maxDecimals uint) decimal.Decimal {
-	if x.IsZero() {
-		return x
+func ToSignificant(input decimal.Decimal, sigDigits int32, maxScale int32) decimal.Decimal {
+	if input.Equal(decimal.Zero) {
+		return input
 	}
 
-	bytes := []byte(x.String())
-	dot := strings.IndexRune(unsafeToString(bytes), '.')
-	p := params{
-		digitsBefore:      getDigitsBefore(bytes, dot),
-		dot:               dot,
-		significantDigits: int(significantDigits),
-		maxDecimals:       int(maxDecimals),
-	}
-
-	if p.digitsBefore > p.significantDigits {
-		roundInt(bytes, p.digitsBefore-p.significantDigits)
-		return buildDecimal(bytes)
-	} else if p.digitsBefore > 0 && p.dot == -1 {
-		// For some minuscule numbers like `math.SmallestNonzeroFloat64`
-		// string representation may be different from its decimal representation,
-		// So it's better to rebuild it from scratch.
-		return buildDecimal(bytes)
-	}
-
-	if truncated := truncateFloat(bytes, p); truncated != nil {
-		bytes = truncated
-	}
-	p.leadingZeros = getLeadingZeros(bytes, p.dot)
-
-	// Calculate the position of the last significant digit
-	if p.digitsBefore > 0 {
-		p.roundTo = int(significantDigits) - p.digitsBefore - p.leadingZeros
-		if p.roundTo < 0 {
-			p.roundTo = p.dot + 1
-		}
+	integralDigits := int32(len(input.Abs().Coefficient().String()))
+	scale := input.Exponent()
+	if scale > 0 {
+		scale = 0
 	} else {
-		p.roundTo = p.dot + p.leadingZeros + int(significantDigits)
-		if p.roundTo > int(maxDecimals) {
-			p.roundTo = int(maxDecimals)
-		}
+		scale = -scale
 	}
 
-	// Make sure rounding occurs inside buffer
-	if p.roundTo > len(bytes) { // e.g. 0.0001
-		p.roundTo = len(bytes) - 1
+	adjustedScale := sigDigits - (integralDigits - scale)
+	if adjustedScale < 0 {
+		// If the number of integral digits is greater than significant digits,
+		// round the number to a scale that maintains the significant digits in the integral part.
+		roundedValue := input.RoundBank(0)
+		multiplier := decimal.NewFromInt(int64(math.Pow10(int(-adjustedScale))))
+		return roundedValue.DivRound(multiplier, 0).Mul(multiplier)
+	} else if adjustedScale > maxScale {
+		adjustedScale = maxScale
 	}
 
-	roundFloat(bytes, p)
-	return buildDecimal(bytes)
-}
-
-// unsafeToString effectively tricks the Go runtime into treating
-// the underlying memory of the byte slice as if it were a string
-// to avoid additional allocation.
-//
-// !!! DO NOT MODIFY STRING YOU GET BACK FROM THIS FUNCTION !!!
-//
-// Since strings in Go are immutable,
-// **YOU MUST ENSURE THAT THE ORIGINAL BYTE SLICE ISN'T MODIFIED** after this conversion,
-// or else you risk causing undefined behavior.
-func unsafeToString(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}
-
-func getDigitsBefore(bytes []byte, dot int) int {
-	if dot == -1 {
-		return len(bytes)
-	}
-
-	var digitsBefore int
-	for _, b := range bytes[:dot] {
-		if b == '0' {
-			continue
-		}
-		digitsBefore++
-	}
-	return digitsBefore
-}
-
-func getLeadingZeros(bytes []byte, dot int) int {
-	var leadingZeros int
-	for _, b := range bytes[dot+1:] {
-		if b != '0' {
-			continue
-		}
-		leadingZeros++
-	}
-	return leadingZeros
-}
-
-// truncateFloat truncates decimals that are greater than max allowed decimals
-func truncateFloat(bytes []byte, p params) []byte {
-	bytesBeforeDot := p.dot + 1
-	bytesAfterDot := len(bytes) - bytesBeforeDot
-	if bytesAfterDot > p.maxDecimals {
-		return bytes[:bytesBeforeDot+p.maxDecimals]
-	}
-	return nil
-}
-
-func roundInt(bytes []byte, to int) {
-	bytesLen := len(bytes)
-	for i := bytesLen - 1; i > bytesLen-to-2; i-- {
-		if bytes[i] == '9' {
-			bytes[i] = '0'
-		} else {
-			bytes[i]++
-
-			if i != bytesLen-1 {
-				bytes[i+1] = '0'
-			}
-		}
-	}
-}
-
-func roundFloat(bytes []byte, p params) {
-	existingSignificantDigits := len(bytes[p.dot+1+p.leadingZeros:])
-	enoughDecimals := p.leadingZeros+existingSignificantDigits <= p.maxDecimals
-	if p.digitsBefore == 0 && enoughDecimals && existingSignificantDigits <= p.significantDigits {
-		// No rounding is required for 0.0001,
-		// as it already has enough significant digits
-		// and there's nothing to round
-		return
-	}
-
-	// Carry mechanism to round up
-	var carry bool
-	for i := len(bytes) - 1; i > p.roundTo-1; i-- {
-		if bytes[i] == '9' {
-			bytes[i] = '0'
-			carry = true
-		} else if bytes[i] >= '5' {
-			bytes[i] = '0'
-			carry = true
-		} else if carry {
-			bytes[i]++
-			carry = false
-		} else {
-			bytes[i] = '0'
-		}
-	}
-
-	// If there's still a carry after the dot, increment the part before the dot
-	if carry {
-		i := p.dot - 1
-		for i >= 0 {
-			if bytes[i] == '9' {
-				bytes[i] = '0'
-				i--
-			} else {
-				bytes[i]++
-				break
-			}
-		}
-		// If we've gone through all the bytes, and they were all '9'
-		if i == -1 {
-			bytes = append([]byte{'1'}, bytes...)
-		}
-	}
-}
-
-func buildDecimal(bytes []byte) decimal.Decimal {
-	result, err := decimal.NewFromString(unsafeToString(bytes))
-	if err != nil {
-		panic("failed to parse converted bytes to decimal")
-	}
-	return result
+	return input.RoundBank(adjustedScale)
 }
--- pkg/precision/precision_test.go
+++ pkg/precision/precision_test.go
@@ -1,4 +1,4 @@
-package precision 
+package precision
 
 import (
 	"fmt"
@@ -9,61 +9,155 @@ import (
 )
 
 func TestToSignificant(t *testing.T) {
+	t.Parallel()
+
 	tests := []struct {
-		input    decimal.Decimal
-		expected decimal.Decimal
+		input  decimal.Decimal
+		expect decimal.Decimal
 	}{
-		{decimal.NewFromFloat(0.0), decimal.NewFromFloat(0.0)},
-		{decimal.NewFromFloat(math.SmallestNonzeroFloat64), decimal.NewFromFloat(0.0)},
-		{decimal.NewFromFloat(0.0001), decimal.NewFromFloat(0.0001)},
-		{decimal.NewFromFloat(0.000123456), decimal.NewFromFloat(0.00012345)},
-		{decimal.NewFromFloat(0.012344789), decimal.NewFromFloat(0.012345)},
-		{decimal.NewFromFloat(0.012345), decimal.NewFromFloat(0.012345)},
-		{decimal.NewFromFloat(1.00000234), decimal.NewFromFloat(1.0)},
-		{decimal.NewFromInt(2), decimal.NewFromInt(2)},
-		{decimal.NewFromInt(100006), decimal.NewFromInt(100010)},
+		{
+			input:  newFromString("0.0"),
+			expect: newFromString("0.0"),
+		}, {
+			input:  decimal.NewFromFloat(math.SmallestNonzeroFloat64),
+			expect: newFromString("0.0"),
+		}, {
+			input:  newFromString("0.0000000000000004"),
+			expect: newFromString("0.0000000000000004"),
+		}, {
+			input:  newFromString("0.00000000000000004"),
+			expect: newFromString("0"),
+		}, {
+			input:  newFromString("0.0001"),
+			expect: newFromString("0.0001"),
+		}, {
+			input:  newFromString("0.000103456"),
+			expect: newFromString("0.00010346"),
+		}, {
+			input:  newFromString("0.012344789"),
+			expect: newFromString("0.012345"),
+		}, {
+			input:  newFromString("0.001023499"),
+			expect: newFromString("0.0010235"),
+		}, {
+			input:  newFromString("0.012345928"),
+			expect: newFromString("0.012346"),
+		}, {
+			input:  newFromString("0.001023499"),
+			expect: newFromString("0.0010235"),
+		}, {
+			input:  newFromString("0.012345"),
+			expect: newFromString("0.012345"),
+		}, {
+			input:  newFromString("1.00000234"),
+			expect: newFromString("1.0"),
+		}, {
+			input:  newFromString("1.0002"),
+			expect: newFromString("1.0002"),
+		}, {
+			input:  newFromString("2"),
+			expect: newFromString("2"),
+		}, {
+			input:  newFromString("100006"),
+			expect: newFromString("100010"),
+		}, {
+			input:  newFromString("12345"),
+			expect: newFromString("12345"),
+		}, {
+			input:  newFromString("222.222222"),
+			expect: newFromString("222.22"),
+		}, {
+			input:  newFromString("2222022.2202"),
+			expect: newFromString("2222000"),
+		}, {
+			input:  newFromString("218166.0002"),
+			expect: newFromString("218170"),
+		},
 	}
 
-	for _, tt := range tests {
-		tt := tt
-		t.Run(fmt.Sprintf("%s -> %s", tt.input, tt.expected), func(t *testing.T) {
-			actual := ToSignificant(tt.input, 5, 8)
-			if !actual.Equals(tt.expected) {
-				t.Errorf("ToSignificant(%s): expected %s, got %s", tt.input, tt.expected, actual)
+	for _, test := range tests {
+		tt := test
+		t.Run(fmt.Sprintf("%s -> %s", tt.input, tt.expect), func(t *testing.T) {
+			t.Parallel()
+
+			actual := ToSignificant(tt.input, 5, 16)
+			if !actual.Equals(tt.expect) {
+				t.Errorf("ToSignificant(%s): expected %s, got %s", tt.input, tt.expect, actual)
+			}
+		})
+
+		ttNeg := test
+		ttNeg.input = ttNeg.input.Neg()
+		ttNeg.expect = ttNeg.expect.Neg()
+		t.Run(fmt.Sprintf("%s -> %s", ttNeg.input, ttNeg.expect), func(t *testing.T) {
+			t.Parallel()
+
+			actual := ToSignificant(ttNeg.input, 5, 16)
+			if !actual.Equals(ttNeg.expect) {
+				t.Errorf("ToSignificant(%s): expected %s, got %s", ttNeg.input, ttNeg.expect, actual)
 			}
 		})
 	}
 }
 
 func BenchmarkToSignificant_DecimalWithLeadingZeros(b *testing.B) {
+	d := newFromString("0.000123456")
+
 	// Reset timer to exclude time taken by setup operations
 	// before the actual benchmark begins
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		ToSignificant(decimal.NewFromFloat(0.000123456), 5, 8)
+		ToSignificant(d, 5, 16)
 	}
 }
 
 func BenchmarkToSignificant_TruncateDecimals(b *testing.B) {
+	d := newFromString("1.00000234")
+
 	// Reset timer to exclude time taken by setup operations
 	// before the actual benchmark begins
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		ToSignificant(decimal.NewFromFloat(1.00000234), 5, 8)
+		ToSignificant(d, 5, 16)
+	}
+}
+
+func BenchmarkToSignificant_ExactSignificantDigits(b *testing.B) {
+  num := "12345"
+	d := newFromString(num)
+  sigDigits := int32(len(num))
+
+	// Reset timer to exclude time taken by setup operations
+	// before the actual benchmark begins
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ToSignificant(d, sigDigits, 16)
 	}
 }
 
 func BenchmarkToSignificant_IntegralPartSizeGreaterThanSignificantDigits(b *testing.B) {
+	d := newFromString("100006")
+
 	// Reset timer to exclude time taken by setup operations
 	// before the actual benchmark begins
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		ToSignificant(decimal.NewFromInt(100006), 5, 8)
+		ToSignificant(d, 5, 16)
 	}
+}
+
+func newFromString(num string) decimal.Decimal {
+	d, err := decimal.NewFromString(num)
+	if err != nil {
+		panic(err)
+	}
+	return d
 }
