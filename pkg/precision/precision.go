a380c05931ed7ea4d49277586c70743120d1c053
Max Pushkarov <mpushkarov@openware.center>
2023-12-11 18:36:53 +0200

    refactor: simplify verification function

--- pkg/precision/precision.go
+++ pkg/precision/precision.go
@@ -1,37 +1,54 @@
 package precision
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/shopspring/decimal"
 )
 
-func ToSignificant(input decimal.Decimal, sigDigits int32, maxScale int32) decimal.Decimal {
+func Convert(input decimal.Decimal, sigDigits, maxPrecision int32) decimal.Decimal {
 	if input.Equal(decimal.Zero) {
 		return input
 	}
 
 	integralDigits := int32(len(input.Coefficient().String()))
-	scale := input.Exponent()
-	if scale <= 0 {
-		scale = -scale
+	precision := input.Exponent()
+	if precision <= 0 {
+		precision = -precision
 	}
 
-	adjustedScale := sigDigits - (integralDigits - scale)
-	if adjustedScale < 0 {
+	adjustedPrecision := sigDigits - (integralDigits - precision)
+	if adjustedPrecision < 0 {
 		// If the number of integral digits is greater than significant digits,
 		// round the number to a scale that maintains the significant digits in the integral part.
-		multiplier := decimal.NewFromInt(10).Pow(decimal.NewFromInt32(-adjustedScale))
+		multiplier := decimal.NewFromInt(10).Pow(decimal.NewFromInt32(-adjustedPrecision))
 		return input.DivRound(multiplier, 0).Mul(multiplier)
-	} else if adjustedScale > maxScale {
-		adjustedScale = maxScale
+	} else if adjustedPrecision > maxPrecision {
+		adjustedPrecision = maxPrecision
 	}
 
-	return input.RoundBank(adjustedScale)
+	return input.RoundBank(adjustedPrecision)
 }
 
-func IsValid(input decimal.Decimal, sigDigits int32, maxScale int32) bool {
+func Validate(input decimal.Decimal, sigDigits, maxPrecision int32) error {
 	if input.IsNegative() {
-		return false
+		return errors.New("input must not be negative")
 	}
-	result := ToSignificant(input, sigDigits, maxScale)
-	return result.Equal(input)
+
+	precision := int32(input.Exponent())
+	if precision < 0 {
+		precision = -precision
+	}
+
+	if precision > maxPrecision {
+		return fmt.Errorf("input must not exceed max precision (allowed: %d; actual: %d)", maxPrecision, precision)
+	}
+
+	integralDigits := int32(len(input.Coefficient().String()))
+	if integralDigits > sigDigits {
+		return fmt.Errorf("input must not exceed max number of significant digits (allowed: %d; actual: %d)", sigDigits, int32(integralDigits))
+	}
+
+	return nil
 }
--- pkg/precision/precision_test.go
+++ pkg/precision/precision_test.go
@@ -11,109 +11,133 @@ import (
 )
 
 const (
-	sigDigits = 8
-	maxScale  = 18
+	sigDigits    int32 = 8
+	maxPrecision int32 = 18
 )
 
-var tests = []struct {
-	input  decimal.Decimal
-	expect decimal.Decimal
-}{
-	{
-		input:  newFromString("0.0"),
-		expect: newFromString("0.0"),
-	}, {
-		input:  decimal.NewFromFloat(math.SmallestNonzeroFloat64),
-		expect: newFromString("0.0"),
-	}, {
-		input:  newFromString("0.0000000000000004"),
-		expect: newFromString("0.0000000000000004"),
-	}, {
-		input:  newFromString("0.0001"),
-		expect: newFromString("0.0001"),
-	}, {
-		input:  newFromString("0.100103456123"),
-		expect: newFromString("0.10010346"),
-	}, {
-		input:  newFromString("0.012345678999"),
-		expect: newFromString("0.012345679"),
-	}, {
-		input:  newFromString("0.001234023499"),
-		expect: newFromString("0.0012340235"),
-	}, {
-		input:  newFromString("0.012345678928"),
-		expect: newFromString("0.012345679"),
-	}, {
-		input:  newFromString("0.100001023499"),
-		expect: newFromString("0.10000102"),
-	}, {
-		input:  newFromString("0.012345"),
-		expect: newFromString("0.012345"),
-	}, {
-		input:  newFromString("1.0000000234"),
-		expect: newFromString("1.0"),
-	}, {
-		input:  newFromString("1.0002"),
-		expect: newFromString("1.0002"),
-	}, {
-		input:  newFromString("2"),
-		expect: newFromString("2"),
-	}, {
-		input:  newFromString("1000000060"),
-		expect: newFromString("1000000100"),
-	}, {
-		input:  newFromString("12345"),
-		expect: newFromString("12345"),
-	}, {
-		input:  newFromString("222.222222"),
-		expect: newFromString("222.22222"),
-	}, {
-		input:  newFromString("2222022.2202"),
-		expect: newFromString("2222022.2"),
-	}, {
-		input:  newFromString("218166.0002"),
-		expect: newFromString("218166"),
-	},
-}
-
-func TestToSignificant(t *testing.T) {
+func TestConvert(t *testing.T) {
 	t.Parallel()
+
+	var tests = []struct {
+		input  decimal.Decimal
+		expect decimal.Decimal
+	}{
+		{
+			input:  newFromString("0.0"),
+			expect: newFromString("0.0"),
+		}, {
+			input:  decimal.NewFromFloat(math.SmallestNonzeroFloat64),
+			expect: newFromString("0.0"),
+		}, {
+			input:  newFromString("0.0000000000000004"),
+			expect: newFromString("0.0000000000000004"),
+		}, {
+			input:  newFromString("0.0001"),
+			expect: newFromString("0.0001"),
+		}, {
+			input:  newFromString("0.100103456123"),
+			expect: newFromString("0.10010346"),
+		}, {
+			input:  newFromString("0.012345678999"),
+			expect: newFromString("0.012345679"),
+		}, {
+			input:  newFromString("0.001234023499"),
+			expect: newFromString("0.0012340235"),
+		}, {
+			input:  newFromString("0.012345678928"),
+			expect: newFromString("0.012345679"),
+		}, {
+			input:  newFromString("0.100001023499"),
+			expect: newFromString("0.10000102"),
+		}, {
+			input:  newFromString("0.012345"),
+			expect: newFromString("0.012345"),
+		}, {
+			input:  newFromString("1.0000000234"),
+			expect: newFromString("1.0"),
+		}, {
+			input:  newFromString("1.0002"),
+			expect: newFromString("1.0002"),
+		}, {
+			input:  newFromString("2"),
+			expect: newFromString("2"),
+		}, {
+			input:  newFromString("1000000060"),
+			expect: newFromString("1000000100"),
+		}, {
+			input:  newFromString("12345"),
+			expect: newFromString("12345"),
+		}, {
+			input:  newFromString("222.222222"),
+			expect: newFromString("222.22222"),
+		}, {
+			input:  newFromString("2222022.2202"),
+			expect: newFromString("2222022.2"),
+		}, {
+			input:  newFromString("218166.0002"),
+			expect: newFromString("218166"),
+		},
+	}
 
 	for _, test := range tests {
 		tt := test
 		t.Run(fmt.Sprintf("%s -> %s", tt.input, tt.expect), func(t *testing.T) {
 			t.Parallel()
 
-			actual := ToSignificant(tt.input, sigDigits, maxScale)
-			if !actual.Equals(tt.expect) {
-				t.Errorf("ToSignificant(%s): expected %s, got %s", tt.input, tt.expect, actual)
-			}
+			actual := Convert(tt.input, sigDigits, maxPrecision)
+			require.True(t, tt.expect.Equals(actual))
 		})
 	}
 }
 
-func TestIsValid(t *testing.T) {
+func TestValidate(t *testing.T) {
 	t.Parallel()
 
-	for _, test := range tests {
-		tt := test
-		t.Run(fmt.Sprintf("%s -> %s", tt.input, tt.expect), func(t *testing.T) {
-			t.Parallel()
+	t.Run("Should return error on negative numbers", func(t *testing.T) {
+		t.Parallel()
 
-			isValid := IsValid(tt.input, sigDigits, maxScale)
-			expect := tt.input.Equal(tt.expect)
-			require.Equal(t, expect, isValid)
-		})
-	}
+		input := newFromString("-12345")
+		err := Validate(input, sigDigits, maxPrecision)
+		require.Error(t, err)
+	})
 
-	t.Run("Should return false on negative numbers", func(t *testing.T) {
-		d := newFromString("-12345")
-		isValid := IsValid(d, sigDigits, maxScale)
-		require.False(t, isValid)
+	t.Run("Should return error if number of significant digits is greater than allowed", func(t *testing.T) {
+		t.Parallel()
+
+		input := newFromString("1234567890")
+		digits := int32(len(input.Coefficient().String()))
+		require.Greater(t, digits, sigDigits)
+
+		err := Validate(input, sigDigits, maxPrecision)
+		require.Error(t, err)
+	})
+
+	t.Run("Should return error if precision is greater than allowed", func(t *testing.T) {
+		t.Parallel()
+
+		input := newFromString("0.00000000000000012345678")
+		precision := int32(math.Abs(float64(input.Exponent())))
+		require.Greater(t, precision, maxPrecision)
+
+		err := Validate(input, sigDigits, maxPrecision)
+		require.Error(t, err)
+	})
+
+	t.Run("Successful test", func(t *testing.T) {
+		t.Parallel()
+
+		input := newFromString("1.2345")
+		digits := int32(len(input.Coefficient().String()))
+		precision := int32(math.Abs(float64(input.Exponent())))
+		require.LessOrEqual(t, digits, sigDigits)
+		require.LessOrEqual(t, precision, maxPrecision)
+
+		err := Validate(input, sigDigits, maxPrecision)
+		require.NoError(t, err)
 	})
 }
 
-func BenchmarkToSignificant_DecimalWithLeadingZeros(b *testing.B) {
+func BenchmarkConvert_DecimalWithLeadingZeros(b *testing.B) {
 	d := newFromString("0.000123456")
 
 	// Reset timer to exclude time taken by setup operations
@@ -122,11 +146,11 @@ func BenchmarkToSignificant_DecimalWithLeadingZeros(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		ToSignificant(d, sigDigits, maxScale)
+		Convert(d, sigDigits, maxPrecision)
 	}
 }
 
-func BenchmarkToSignificant_TruncateDecimals(b *testing.B) {
+func BenchmarkConvert_TruncateDecimals(b *testing.B) {
 	d := newFromString("1.00000000234")
 
 	// Reset timer to exclude time taken by setup operations
@@ -135,13 +159,13 @@ func BenchmarkToSignificant_TruncateDecimals(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		ToSignificant(d, sigDigits, maxScale)
+		Convert(d, sigDigits, maxPrecision)
 	}
 }
 
-func BenchmarkToSignificant_ExactSignificantDigits(b *testing.B) {
+func BenchmarkConvert_ExactSignificantDigits(b *testing.B) {
 	d := newFromString("12345678")
-	if len(d.String()) != sigDigits {
+	if int32(len(d.String())) != sigDigits {
 		panic("expected number of digits to be equal to number of significant digits")
 	}
 
@@ -151,13 +175,13 @@ func BenchmarkToSignificant_ExactSignificantDigits(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		ToSignificant(d, sigDigits, maxScale)
+		Convert(d, sigDigits, maxPrecision)
 	}
 }
 
-func BenchmarkToSignificant_IntegralPartSizeGreaterThanSignificantDigits(b *testing.B) {
+func BenchmarkConvert_IntegralPartSizeGreaterThanSignificantDigits(b *testing.B) {
 	d := newFromString("1000000060")
-	if integralPart := strings.Split(d.String(), ".")[0]; len(integralPart) <= sigDigits {
+	if integralPart := strings.Split(d.String(), ".")[0]; int32(len(integralPart)) <= sigDigits {
 		panic("expected number of digits to be greater than number of significant digits")
 	}
 
@@ -167,7 +191,7 @@ func BenchmarkToSignificant_IntegralPartSizeGreaterThanSignificantDigits(b *test
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		ToSignificant(d, sigDigits, maxScale)
+		Convert(d, sigDigits, maxPrecision)
 	}
 }
 
