4de621ccbb45e5d00a933294aeb354ba9dbf4295
Max Pushkarov <mpushkarov@openware.center>
2024-01-04 16:19:13 +0200

    refactor(contracts): bump Solidity version from 0.8.17 to ^0.8.20

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,7 +30,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.18',
+        version: '0.8.20',
         settings: {
           optimizer: {
             enabled: true,
