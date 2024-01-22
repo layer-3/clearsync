94d6693566fb0296ffc62b716ecab5a4001a2fea
Anton Filonenko <phil@yellow.org>
2024-01-22 14:42:10 +0200

    fix: SyncSwapRouter binding

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -184,7 +184,7 @@ const config: HardhatUserConfig = {
       '@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol',
       '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol',
       '@syncswap/core-contracts/contracts/interfaces/factory/IBasePoolFactory.sol',
-      '@syncswap/core-contracts/contracts/interfaces/IRouter.sol',
+      '@syncswap/core-contracts/contracts/SyncSwapRouter.sol',
     ],
   },
 };
