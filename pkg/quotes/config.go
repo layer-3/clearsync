fe40862ffd2e574e5ac847f0180d386cda85303c
Max Pushkarov <mpushkarov@openware.center>
2024-02-01 18:20:24 +0200

    refactor(quotes): add common interface that unifies all config types (#192)

--- pkg/quotes/config.go
+++ pkg/quotes/config.go
@@ -18,6 +18,10 @@ type Config struct {
 	Syncswap      SyncswapConfig      `yaml:"syncswap"`
 }
 
+type DriverConfig interface {
+	DriverType() DriverType
+}
+
 func NewConfigFromFile(path string) (Config, error) {
 	var config Config
 	return config, cleanenv.ReadConfig(path, &config)
@@ -28,8 +32,34 @@ func NewConfigFromEnv() (Config, error) {
 	return config, cleanenv.ReadEnv(&config)
 }
 
+func ToConfig(driver DriverConfig) Config {
+	config := Config{Driver: driver.DriverType()}
+
+	switch driver.DriverType() {
+	case DriverBinance:
+		config.Binance = driver.(BinanceConfig)
+	case DriverKraken:
+		config.Kraken = driver.(KrakenConfig)
+	case DriverOpendax:
+		config.Opendax = driver.(OpendaxConfig)
+	case DriverBitfaker:
+		config.Bitfaker = driver.(BitfakerConfig)
+	case DriverUniswapV3Api:
+		config.UniswapV3Api = driver.(UniswapV3ApiConfig)
+	case DriverUniswapV3Geth:
+		config.UniswapV3Geth = driver.(UniswapV3GethConfig)
+	case DriverSyncswap:
+		config.Syncswap = driver.(SyncswapConfig)
+	}
+	return config
+}
+
 type BinanceConfig struct {
 	TradeSampler TradeSamplerConfig `yaml:"trade_sampler"`
+}
+
+func (BinanceConfig) DriverType() DriverType {
+	return DriverBinance
 }
 
 type KrakenConfig struct {
@@ -38,10 +68,18 @@ type KrakenConfig struct {
 	TradeSampler    TradeSamplerConfig `yaml:"trade_sampler"`
 }
 
+func (KrakenConfig) DriverType() DriverType {
+	return DriverKraken
+}
+
 type OpendaxConfig struct {
 	URL             string             `yaml:"url" env:"QUOTES_OPENDAX_URL" env-default:"wss://alpha.yellow.org/api/v1/finex/ws"`
 	ReconnectPeriod time.Duration      `yaml:"period" env:"QUOTES_OPENDAX_RECONNECT_PERIOD" env-default:"5s"`
 	TradeSampler    TradeSamplerConfig `yaml:"trade_sampler"`
+}
+
+func (OpendaxConfig) DriverType() DriverType {
+	return DriverOpendax
 }
 
 type BitfakerConfig struct {
@@ -49,10 +87,18 @@ type BitfakerConfig struct {
 	TradeSampler TradeSamplerConfig `yaml:"trade_sampler"`
 }
 
+func (BitfakerConfig) DriverType() DriverType {
+	return DriverBitfaker
+}
+
 type UniswapV3ApiConfig struct {
 	URL          string             `yaml:"url" env:"QUOTES_UNISWAP_V3_API_URL" env-default:"https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"`
 	WindowSize   time.Duration      `yaml:"window_size" env:"QUOTES_UNISWAP_V3_API_WINDOW_SIZE" env-default:"2s"`
 	TradeSampler TradeSamplerConfig `yaml:"trade_sampler"`
+}
+
+func (UniswapV3ApiConfig) DriverType() DriverType {
+	return DriverUniswapV3Api
 }
 
 type UniswapV3GethConfig struct {
@@ -62,11 +108,19 @@ type UniswapV3GethConfig struct {
 	TradeSampler   TradeSamplerConfig `yaml:"trade_sampler"`
 }
 
+func (UniswapV3GethConfig) DriverType() DriverType {
+	return DriverUniswapV3Geth
+}
+
 type SyncswapConfig struct {
 	URL                       string             `yaml:"url" env:"QUOTES_SYNCSWAP_URL" env-default:""`
 	AssetsURL                 string             `yaml:"assets_url" env:"QUOTES_SYNCSWAP_ASSETS_URL" env-default:"https://raw.githubusercontent.com/layer-3/clearsync/master/networks/mainnet/assets.json"`
 	ClassicPoolFactoryAddress string             `yaml:"classic_pool_factory_address" env:"QUOTES_SYNCSWAP_CLASSIC_POOL_FACTORY_ADDRESS" env-default:"0x37BAc764494c8db4e54BDE72f6965beA9fa0AC2d"`
 	TradeSampler              TradeSamplerConfig `yaml:"trade_sampler"`
+}
+
+func (SyncswapConfig) DriverType() DriverType {
+	return DriverSyncswap
 }
 
 type TradeSamplerConfig struct {
