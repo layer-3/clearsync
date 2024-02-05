269a4001c039d08ac47a5ab331a33ec93b6b8277
MaxMoskalenko <mx.msklnk@gmail.com>
2024-02-05 11:13:17 +0200

    fix: add market helpers

--- pkg/quotes/market.go
+++ pkg/quotes/market.go
@@ -1,6 +1,8 @@
 package quotes
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/shopspring/decimal"
@@ -9,6 +11,30 @@ import (
 type Market struct {
 	BaseUnit  string // e.g. `btc` in `btc/usdt`
 	QuoteUnit string // e.g. `usdt` in `btc/usdt`
+}
+
+// String returns a string representation of the market
+// Market{BTC, Usdt} -> "btc/usdt"
+func (m Market) String() string {
+	return strings.ToLower(fmt.Sprintf("%s/%s", m.BaseUnit, m.QuoteUnit))
+}
+
+func NewMarket(base, quote string) Market {
+	return Market{
+		BaseUnit:  base,
+		QuoteUnit: quote,
+	}
+}
+
+// NewMarketFromString returns a new Market from a string
+// "btc/usdt" -> Market{BTC, Usdt}
+// NOTE: string should contain "/" delimiter
+func NewMarketFromString(s string) (Market, bool) {
+	parts := strings.Split(s, "/")
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return Market{}, false
+	}
+	return NewMarket(parts[0], parts[1]), true
 }
 
 // TradeEvent is a generic container
