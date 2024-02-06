a91658468e12729e3dcfa749c672aedbd0c56bbe
Max Pushkarov <mpushkarov@openware.center>
2024-02-06 15:06:32 +0200

    feat(safe): use mutex instead of spinlock for synchronization

--- pkg/safe/map.go
+++ pkg/safe/map.go
@@ -1,28 +1,27 @@
 package safe
 
-// Thread safe generic map. Uses a RWMutex to allow concurrent reads and
-// exclusive writes.
-type Map[K comparable, V any] struct {
-	m map[K]V
+import "sync"
 
-	mu RWSpinLock
+// Map is a thread safe generic map.
+type Map[K comparable, V any] struct {
+	m  map[K]V
+	mu sync.RWMutex
 }
 
-// New creates a new thread safe map.
+// NewMap creates a new thread safe map.
 func NewMap[K comparable, V any]() Map[K, V] {
 	return Map[K, V]{
 		m: make(map[K]V),
 	}
 }
 
+// NewMapWithData creates a new thread safe map with initial data.
 func NewMapWithData[K comparable, V any](data map[K]V) Map[K, V] {
-	return Map[K, V]{
-		m: data,
-	}
+	return Map[K, V]{m: data}
 }
 
-// Load returns the value stored in the map for a key, or false if no value is
-// present.
+// Load returns the value stored in the map for a key,
+// otherwise false if no value is present.
 func (m *Map[K, V]) Load(k K) (V, bool) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
@@ -47,8 +46,9 @@ func (m *Map[K, V]) Delete(k K) {
 	delete(m.m, k)
 }
 
-// Range iterates over the map, calling the provided function for each key/value
-// pair. If the function returns false, the iteration stops.
+// Range iterates over the map, calling the provided function
+// for each key/value pair. If the function returns false,
+// the iteration stops.
 func (m *Map[K, V]) Range(f func(k K, v V) bool) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
