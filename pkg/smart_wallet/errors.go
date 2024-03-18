0adf5e1b2f077c4dd96e8fecf37b1f23cd0e8e8b
nksazonov <nsazonov@openware.com>
2024-03-18 12:49:51 +0200

    ref: update comment on ErrSmartWalletNotSupported error

--- pkg/smart_wallet/errors.go
+++ pkg/smart_wallet/errors.go
@@ -3,8 +3,8 @@ package smart_wallet
 import "errors"
 
 var (
-	// ErrSmartWalletNotSupported is returned on attempt to build client with an unsupported smart wallet type.
+	// ErrSmartWalletNotSupported is returned on attempt to perform some actions with an unsupported smart wallet type.
 	// Make sure that the smart wallet type you are trying to use
-	// has no `unsupported` or `untested` tags in `wallet_type.go` source file.
+	// has no `unsupported` or `untested` tags in the source files.
 	ErrSmartWalletNotSupported = errors.New("smart wallet not supported")
 )
