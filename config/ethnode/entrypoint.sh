0931c869f9ed630b1e4ceac2befe3dab0a0720b1
MaxMoskalenko <mx.msklnk@gmail.com>
2024-03-28 18:08:04 +0200

    fix: use anvil instead of geth

--- config/ethnode/entrypoint.sh
+++ config/ethnode/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nohup geth --dev --http --ws --dev.period=1 --http.api=eth,web3,net,personal --allow-insecure-unlock --http.addr=0.0.0.0 --http.corsdomain='*' --http.vhosts='*' --ws.addr=0.0.0.0 --ws.origins='*' &
+nohup anvil --host "0.0.0.0" & 
 
 while ! nc -z localhost 8545; do
   sleep 1
