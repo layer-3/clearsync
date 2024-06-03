86d2b0a0f36a71d2e0f09d66e123b82b81321aab
Anton Filonenko <phil@yellow.org>
2024-06-03 16:54:22 +0300

    fix: wait for ethnode correctly (#367)

--- config/ethnode/deploy-contracts.js
+++ config/ethnode/deploy-contracts.js
@@ -1,7 +1,7 @@
 let fs = require('fs');
 var { Web3 } = require('web3');
 
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+var web3 = new Web3(new Web3.providers.HttpProvider('http://0.0.0.0:8545'));
 let eth = web3.eth;
 
 let deployerAccount;
--- config/ethnode/entrypoint.sh
+++ config/ethnode/entrypoint.sh
@@ -2,12 +2,12 @@
 
 nohup anvil --host "0.0.0.0" & 
 
-while ! nc -z localhost 8545; do
+while ! nc -z "0.0.0.0" 8545; do
   sleep 1
 done
 
 NODE_PATH=$(npm root -g) node /app/deploy-contracts.js
 
-while nc -z localhost 8545; do
+while nc -z "0.0.0.0" 8545; do
   sleep 1
 done
