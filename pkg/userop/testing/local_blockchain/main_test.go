5d06e0c6e94d3ed85ad854af1eb741d7c8ec4561
MaxMoskalenko <mx.msklnk@gmail.com>
2024-09-05 16:05:03 +0300

    feat: add support of Paymaster for local env

--- pkg/userop/testing/local_blockchain/main_test.go
+++ pkg/userop/testing/local_blockchain/main_test.go
@@ -6,10 +6,12 @@ import (
 	"os"
 	"testing"
 
+	"github.com/layer-3/clearsync/pkg/artifacts/test_erc20"
 	"github.com/layer-3/clearsync/pkg/signer"
 	"github.com/layer-3/clearsync/pkg/smart_wallet"
 	"github.com/layer-3/clearsync/pkg/userop"
 	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -41,13 +43,15 @@ func TestSimulatedRPC(t *testing.T) {
 
 	// 3. Start the bundler
 	for i := 0; i < 3; i++ { // starting multiple bundlers to test reusing existing bundlers
-		bundlerURL := NewBundler(ctx, t, node, addresses.EntryPoint)
-		slog.Info("connecting to bundler", "bundlerURL", bundlerURL.String())
+		bundler := NewBundler(ctx, t, node, addresses.EntryPoint)
+		slog.Info("connecting to bundler", "bundlerURL", bundler.LocalURL.String())
 	}
-	bundlerURL := *NewBundler(ctx, t, node, addresses.EntryPoint)
+	bundler := NewBundler(ctx, t, node, addresses.EntryPoint)
 
 	// 4. Build client
-	client := buildClient(t, node.LocalURL, bundlerURL, addresses)
+	client := buildClient(t, node.LocalURL, bundler.LocalURL, addresses, userop.PaymasterConfig{
+		Type: &userop.PaymasterDisabled,
+	})
 
 	// 5. Create and fund smart account
 	eoa, receiver, swAddress := setupAccounts(ctx, t, client, node)
@@ -71,4 +75,60 @@ func TestSimulatedRPC(t *testing.T) {
 	receiverBalance, err := node.Client.BalanceAt(ctx, receiver.Address, nil)
 	require.NoError(t, err, "failed to fetch receiver new balance")
 	require.Equal(t, transferAmount, receiverBalance, "new balance should equal the transfer amount")
+}
+
+func TestSimulatedPaymaster(t *testing.T) {
+	setLogLevel(slog.LevelDebug)
+	ctx := context.Background()
+
+	node := NewEthNode(ctx, t)
+	slog.Info("connecting to Ethereum node", "rpcURL", node.LocalURL.String())
+
+	// Deploy the required contracts
+	addresses := SetupContracts(ctx, t, node)
+
+	bundler := NewBundler(ctx, t, node, addresses.EntryPoint)
+	balance := decimal.NewFromFloat(50e18 /* 50 ETH */).BigInt()
+	owner, err := NewAccountWithBalance(ctx, balance, node)
+	require.NoError(t, err, "failed to create owner account")
+
+	// Deploy ERC20 token
+	token, _, _, err := test_erc20.DeployTestERC20(owner.TransactOpts, node.Client, "TestToken", "TT", 6, big.NewInt(1000*1e6)) // 1000 tokens
+	require.NoError(t, err, "failed to deploy ERC20 token")
+
+	// Deploy paymaster
+	paymasterURL := SetupPaymaster(ctx, t, node, bundler, token)
+
+	// Build client
+	client := buildClient(t, node.LocalURL, bundler.LocalURL, addresses, userop.PaymasterConfig{
+		Type:    &userop.PaymasterPimlicoVerifying,
+		URL:     paymasterURL.String(),
+		Address: addresses.Paymaster,
+	})
+
+	// Create smart account
+	eoa, err := NewAccount(ctx, node) // EOA without funds
+	require.NoError(t, err, "failed to create EOA")
+	slog.Info("eoa", "address", eoa.Address)
+
+	swAddress, err := client.GetAccountAddress(ctx, eoa.Address, decimal.Zero)
+	sw := Account{Address: swAddress}
+	require.NoError(t, err, "failed to compute sender account address")
+	slog.Info("sender", "address", sw.Address)
+
+	// Send userop
+	signer := userop.SignerForKernel(signer.NewLocalSigner(eoa.PrivateKey))
+	transferAmount := decimal.NewFromInt(0 /* 0 wei */).BigInt()
+	calls := smart_wallet.Calls{{To: sw.Address, Value: transferAmount}}
+	params := &userop.WalletDeploymentOpts{Index: decimal.Zero, Owner: eoa.Address}
+	op, err := client.NewUserOp(ctx, sw.Address, signer, calls, params, nil)
+	assert.NoError(t, err, "failed to create new user operation")
+	slog.Info("ready to send", "userop", op)
+
+	done, err := client.SendUserOp(ctx, op)
+	assert.NoError(t, err, "failed to send user operation")
+
+	receipt := <-done
+	slog.Info("transaction mined", "receipt", receipt)
+	assert.True(t, receipt.Success)
 }
--- pkg/userop/testing/local_blockchain/node.go
+++ pkg/userop/testing/local_blockchain/node.go
@@ -19,6 +19,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
@@ -33,8 +34,13 @@ import (
 	"github.com/layer-3/clearsync/pkg/userop"
 )
 
+// These addresses are hardcoded in the Paymaster contract and determined by CREATE2
+const ENTRY_POINT_ADDRESS_V06 = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
+const VERIFYING_PAYMASTER_ADDRESS_V06 = "0x28ec0633192d0cBd9E1156CE05D5FdACAcB93947"
+
 type EthNode struct {
 	Container    testcontainers.Container
+	RPCClient    *rpc.Client
 	Client       *ethclient.Client
 	LocalURL     url.URL
 	ContainerURL url.URL
@@ -126,11 +132,11 @@ func NewEthNode(ctx context.Context, t *testing.T) *EthNode {
 	}
 
 	var (
-		err           error
-		gethContainer testcontainers.Container
-		ethClient     *ethclient.Client
-		rpcURL        *url.URL
-		containerURL  *url.URL
+		err            error
+		anvilContainer testcontainers.Container
+		ethClient      *ethclient.Client
+		rpcURL         *url.URL
+		containerURL   *url.URL
 	)
 
 	if u := os.Getenv("GETH_NODE_RPC_URL"); u != "" {
@@ -140,8 +146,9 @@ func NewEthNode(ctx context.Context, t *testing.T) *EthNode {
 	} else {
 		// TODO: use in-memory database instead of container volumes
 		// TODO: add test cleanups with container termination
-		gethContainer, err = testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		anvilContainer, err = testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 			ContainerRequest: testcontainers.ContainerRequest{
+				Name:  "anvil",
 				Image: "quay.io/openware/geth:v0.1.7-amd64",
 				// 8545 TCP, used by the HTTP based JSON RPC API
 				// 8546 TCP, used by the WebSocket based JSON RPC API
@@ -161,11 +168,11 @@ func NewEthNode(ctx context.Context, t *testing.T) *EthNode {
 		// Hardcoded deployer private key for testing purposes
 		os.Setenv("DEPLOYER_PK", "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
 
-		containerIP, err := gethContainer.ContainerIP(ctx)
+		containerIP, err := anvilContainer.ContainerIP(ctx)
 		require.NoError(t, err, "failed to get Go-Ethereum container IP")
 		// As a rpc port we are using ws port for subscription
 		// As a container port we are using http port for bundler
-		rpcPort, err := gethContainer.MappedPort(ctx, "8545")
+		rpcPort, err := anvilContainer.MappedPort(ctx, "8545")
 		require.NoError(t, err, "failed to get Go-Ethereum rpc port")
 
 		rpcURL, err = url.Parse(fmt.Sprintf("ws://0.0.0.0:%s", rpcPort.Port()))
@@ -174,13 +181,16 @@ func NewEthNode(ctx context.Context, t *testing.T) *EthNode {
 		require.NoError(t, err, "failed to parse container RPC URL")
 	}
 
-	ethClient, err = ethclient.Dial(rpcURL.String())
+	rpcClient, err := rpc.DialContext(context.Background(), rpcURL.String())
 	require.NoError(t, err)
+
+	ethClient = ethclient.NewClient(rpcClient)
 
 	slog.Info("Go-Ethereum container started", "rpcURL", rpcURL.String())
 	ethActiveNode = &EthNode{
-		Container:    gethContainer,
+		Container:    anvilContainer,
 		Client:       ethClient,
+		RPCClient:    rpcClient,
 		LocalURL:     *rpcURL,
 		ContainerURL: *containerURL,
 	}
@@ -190,7 +200,8 @@ func NewEthNode(ctx context.Context, t *testing.T) *EthNode {
 
 type BundlerNode struct {
 	Container    testcontainers.Container
-	ContainerURL *url.URL
+	LocalURL     url.URL
+	ContainerURL url.URL
 }
 
 var (
@@ -198,29 +209,31 @@ var (
 	bundlerMutex      sync.Mutex
 )
 
-func NewBundler(ctx context.Context, t *testing.T, node *EthNode, entryPoint common.Address) *url.URL {
+func NewBundler(ctx context.Context, t *testing.T, node *EthNode, entryPoint common.Address) *BundlerNode {
 	bundlerMutex.Lock()
 	defer bundlerMutex.Unlock()
 	slog.Info("starting bundler...")
 
 	if bundlerActiveNode != nil {
 		slog.Info("reusing existing Alto bundler")
-		return bundlerActiveNode.ContainerURL
+		return bundlerActiveNode
 	}
 
 	var (
+		err           error
 		altoContainer testcontainers.Container
-		bundlerURL    *url.URL
+		rpcURL        *url.URL
+		containerURL  *url.URL
 	)
 
 	if uEnv := os.Getenv("BUNDLER_RPC_URL"); uEnv != "" {
-		u, err := url.Parse(uEnv)
+		rpcURL, err = url.Parse(uEnv)
 		if err != nil {
 			// configuration error, so we shut down the app
 			panic(err)
 		}
 
-		bundlerURL = u
+		containerURL = rpcURL
 	} else {
 		const port = "3000"
 		balance := decimal.NewFromFloat(100e18 /* 100 ETH */).BigInt()
@@ -252,28 +265,38 @@ func NewBundler(ctx context.Context, t *testing.T, node *EthNode, entryPoint com
 		})
 		require.NoError(t, err, "failed to start Alto bundler container")
 
-		containerPort, err := altoContainer.MappedPort(ctx, port)
-		require.NoError(t, err, "failed to get Alto bundler container port")
+		containerIP, err := altoContainer.ContainerIP(ctx)
+		require.NoError(t, err, "failed to get Go-Ethereum container IP")
+		// As a rpc port we are using ws port for subscription
+		// As a container port we are using http port for other services
+		rpcPort, err := altoContainer.MappedPort(ctx, port)
+		require.NoError(t, err, "failed to get Go-Ethereum rpc port")
 
-		bundlerURL, err = url.Parse(fmt.Sprintf("http://0.0.0.0:%s", containerPort.Port()))
-		require.NoError(t, err, "failed to parse local bundler URL")
+		rpcURL, err = url.Parse(fmt.Sprintf("http://0.0.0.0:%s", rpcPort.Port()))
+		require.NoError(t, err, "failed to parse local RPC URL")
+
+		containerURL, err = url.Parse(fmt.Sprintf("http://%s:%s", containerIP, rpcPort.Port()))
+		require.NoError(t, err, "failed to parse container RPC URL")
 	}
 
 	bundlerActiveNode = &BundlerNode{
 		Container:    altoContainer,
-		ContainerURL: bundlerURL,
+		ContainerURL: *containerURL,
+		LocalURL:     *rpcURL,
 	}
 
-	return bundlerActiveNode.ContainerURL
+	return bundlerActiveNode
 }
 
 type Contracts struct {
-	EntryPoint          common.Address
-	ECDSAValidator      common.Address
-	SessionKeyValidator common.Address
-	Logic               common.Address
-	Factory             common.Address
-	Paymaster           common.Address
+	EntryPoint            common.Address
+	ECDSAValidator        common.Address
+	SessionKeyValidator   common.Address
+	Logic                 common.Address
+	Factory               common.Address
+	Paymaster             common.Address
+	PaymasterManualOracle common.Address
+	PaymasterOwner        common.Address
 }
 
 var (
@@ -302,6 +325,15 @@ func SetupContracts(ctx context.Context, t *testing.T, node *EthNode) Contracts 
 
 	entryPoint, _, _, err := entry_point_v0_6_0.DeployEntryPoint(owner.TransactOpts, node.Client)
 	require.NoError(t, err)
+	// NOTE: it will only work with local anvil node
+	// Setting code for the hardcoded entry point address
+	entryPointCode, err := node.Client.CodeAt(ctx, entryPoint, nil)
+	require.NoError(t, err)
+
+	err = node.RPCClient.CallContext(ctx, nil, "anvil_setCode", ENTRY_POINT_ADDRESS_V06, hexutil.Encode(entryPointCode))
+	require.NoError(t, err)
+
+	entryPoint = common.HexToAddress(ENTRY_POINT_ADDRESS_V06)
 	slog.Info("deployed EntryPoint contract", "address", entryPoint)
 
 	ecdsaValidator, _, _, err := kernel_ecdsa_validator_v2_2.DeployKernelECDSAValidator(owner.TransactOpts, node.Client)
@@ -324,8 +356,6 @@ func SetupContracts(ctx context.Context, t *testing.T, node *EthNode) Contracts 
 	require.NoError(t, err)
 	slog.Info("set Kernel implementation", "tx", tx.Hash().Hex())
 
-	var paymaster common.Address // TODO: deploy Paymaster contract
-
 	slog.Info("done deploying contracts")
 	contracts := Contracts{
 		EntryPoint:          entryPoint,
@@ -333,7 +363,7 @@ func SetupContracts(ctx context.Context, t *testing.T, node *EthNode) Contracts 
 		SessionKeyValidator: sessionKeyValidator,
 		Logic:               logic,
 		Factory:             factory,
-		Paymaster:           paymaster,
+		Paymaster:           common.HexToAddress(VERIFYING_PAYMASTER_ADDRESS_V06),
 	}
 
 	cachedContracts = &contracts
@@ -352,7 +382,7 @@ func getContractAddressesFromEnv() Contracts {
 	}
 }
 
-func defaultClientConfig(t *testing.T, rpcURL, bundlerURL url.URL, addresses Contracts) userop.ClientConfig {
+func defaultClientConfig(t *testing.T, rpcURL, bundlerURL url.URL, addresses Contracts, paymasterConfig userop.PaymasterConfig) userop.ClientConfig {
 	config, err := userop.NewClientConfigFromEnv()
 	require.NoError(t, err)
 
@@ -365,15 +395,63 @@ func defaultClientConfig(t *testing.T, rpcURL, bundlerURL url.URL, addresses Con
 		Logic:          addresses.Logic,
 		Factory:        addresses.Factory,
 	}
-	config.Paymaster.Type = &userop.PaymasterDisabled
-	config.Paymaster.Address = addresses.Paymaster
+	config.Paymaster = paymasterConfig
 
 	return config
 }
 
-func buildClient(t *testing.T, rpcURL, bundlerURL url.URL, addresses Contracts) userop.Client {
-	config := defaultClientConfig(t, rpcURL, bundlerURL, addresses)
+func buildClient(t *testing.T, rpcURL, bundlerURL url.URL, addresses Contracts, paymasterConfig userop.PaymasterConfig) userop.Client {
+	config := defaultClientConfig(t, rpcURL, bundlerURL, addresses, paymasterConfig)
 	client, err := userop.NewClient(config)
 	require.NoError(t, err)
 	return client
+}
+
+func SetupPaymaster(ctx context.Context, t *testing.T, node *EthNode, bundler *BundlerNode, tokenAddress common.Address) *url.URL {
+	deployDeterministicDeployer(ctx, t, node)
+
+	const port = "3001"
+	paymasterContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: "quay.io/openware/paymaster:v0.1.0-mock-amd64",
+			Env: map[string]string{
+				"ALTO_RPC":  bundler.ContainerURL.String(),
+				"ANVIL_RPC": node.ContainerURL.String(),
+				"PORT":      port,
+			},
+			ImagePlatform: "linux/amd64",
+			ExposedPorts:  []string{fmt.Sprintf("%s:%s/tcp", port, port)},
+			WaitingFor:    wait.ForLog("Listening on"),
+		},
+		Started: true,
+	})
+	require.NoError(t, err, "failed to start Paymaster container")
+
+	containerPort, err := paymasterContainer.MappedPort(ctx, port)
+	require.NoError(t, err, "failed to get Paymaster container port")
+
+	paymasterURL, err := url.Parse(fmt.Sprintf("http://0.0.0.0:%s", containerPort.Port()))
+	require.NoError(t, err, "failed to parse local Paymaster URL")
+
+	return paymasterURL
+}
+
+var (
+	isDeterministicDeployerDeployed = false
+)
+
+// NOTE: to get the idea about deploying deterministic deployer, check the following
+// page: https://github.com/Arachnid/deterministic-deployment-proxy/blob/master/README.md
+func deployDeterministicDeployer(ctx context.Context, t *testing.T, node *EthNode) {
+	if isDeterministicDeployerDeployed {
+		return
+	}
+
+	err := node.RPCClient.CallContext(ctx, nil, "anvil_setBalance", "0x3fab184622dc19b6109349b94811493bf2a45362", fmt.Sprintf("%x", big.NewInt(1e18).String()))
+	require.NoError(t, err)
+
+	err = node.RPCClient.CallContext(ctx, nil, "eth_sendRawTransaction", "0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222")
+	require.NoError(t, err)
+
+	isDeterministicDeployerDeployed = true
 }
