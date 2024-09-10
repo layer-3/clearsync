59dec42adac9b3095e058719660642247735256c
Sazonov Nikita <35502225+nksazonov@users.noreply.github.com>
2024-09-10 13:00:06 +0300

    feat: fetch gas prices from gas station for Polygon chains (#424)
    * feat: fetch gas prices from gas station for polygon chains
    
    * ref: extract gas-related code to gas.go
    
    * fix: convert fetched prices from float to big.Int
    
    * ref: apply comments

---
+++ pkg/userop/gas_test.go
@@ -0,0 +1,39 @@
+package userop
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetPolygonGasPrices(t *testing.T) {
+	t.Run("Should return gas prices for Polygon", func(t *testing.T) {
+		chainId := big.NewInt(137)
+		maxFee, maxPriorityFee, err := getPolygonGasPrices(chainId)
+		require.NoError(t, err)
+		require.NotNil(t, maxFee)
+		require.NotNil(t, maxPriorityFee)
+		// Gas station states that for Polygon Mainnet the maxPriorityFee is at least 30 Gwei
+		require.True(t, maxPriorityFee.Cmp(big.NewInt(30).Exp(big.NewInt(10), big.NewInt(9), nil)) >= 0)
+	})
+
+	t.Run("Should return gas prices for Amoy", func(t *testing.T) {
+		chainId := big.NewInt(80002)
+		maxFee, maxPriorityFee, err := getPolygonGasPrices(chainId)
+		require.NoError(t, err)
+		require.NotNil(t, maxFee)
+		require.NotNil(t, maxPriorityFee)
+	})
+
+	t.Run("Should return error for other chain", func(t *testing.T) {
+		chainId := big.NewInt(42)
+		_, _, err := getPolygonGasPrices(chainId)
+		require.Error(t, err)
+	})
+
+	t.Run("Should return error if chainId is nil", func(t *testing.T) {
+		_, _, err := getPolygonGasPrices(nil)
+		require.Error(t, err)
+	})
+}
--- pkg/userop/middleware.go
+++ pkg/userop/middleware.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"math/big"
-	"strconv"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -82,102 +81,51 @@ func getGasPricesMiddleware(provider EthBackend, gasConfig GasConfig) middleware
 
 		logger.Debug("getting gas prices")
 
-		// Calculate maxPriorityFeePerGas
 		var maxPriorityFeePerGas *big.Int
-		if !op.MaxPriorityFeePerGas.IsZero() {
-			maxPriorityFeePerGas = op.MaxPriorityFeePerGas.BigInt()
-		} else {
-			var maxPriorityFeePerGasStr string
-			if err := provider.RPC().CallContext(ctx, &maxPriorityFeePerGasStr, "eth_maxPriorityFeePerGas"); err != nil {
-				return err
-			}
-
-			var ok bool
-			maxPriorityFeePerGas, ok = new(big.Int).SetString(maxPriorityFeePerGasStr, 0)
-			if !ok {
-				return fmt.Errorf("failed to parse maxPriorityFeePerGas: %s", maxPriorityFeePerGasStr)
-			}
-			logger.Debug("fetched maxPriorityFeePerGas", "maxPriorityFeePerGas", maxPriorityFeePerGas.String())
-
-			// Increase maxPriorityFeePerGas to give user more
-			// flexibility in setting the gas price.
-			maxPriorityFeePerGas.Mul(
-				maxPriorityFeePerGas,
-				gasConfig.MaxPriorityFeePerGasMultiplier.BigInt())
-		}
-
-		// Calculate maxFeePerGas
 		var maxFeePerGas *big.Int
-		if !op.MaxFeePerGas.IsZero() {
-			maxFeePerGas = op.MaxFeePerGas.BigInt()
-		} else {
-			// Get the latest block to read its base fee
-			block, err := provider.BlockByNumber(ctx, nil)
-			if err != nil {
-				return err
-			}
-			blockBaseFee := block.BaseFee()
-			logger.Debug("fetched block base fee", "baseFee", blockBaseFee.String())
 
-			maxFeePerGas = new(big.Int).Mul(blockBaseFee, gasConfig.MaxFeePerGasMultiplier.BigInt())
-			maxFeePerGas.Add(maxFeePerGas, maxPriorityFeePerGas)
+		chainId, err := provider.ChainID(ctx)
+		if err != nil || chainId == nil {
+			return fmt.Errorf("failed to get chain ID: %w", err)
 		}
+
+		isPolygon := chainId.Uint64() == 137 || chainId.Uint64() == 80002
+
+		// for Polygon and Amoy, fetch from polygon gas station
+		if isPolygon {
+			maxFeePerGas, maxPriorityFeePerGas, err = getPolygonGasPrices(chainId)
+			if err != nil {
+				logger.Error("failed to get gas prices from polygon gas station", "error", err)
+			}
+		}
+
+		// for other chains, or in case gas station is down, fetch from provider
+		if !isPolygon || err != nil {
+			maxFeePerGas, maxPriorityFeePerGas, err = getGasPrices(ctx, provider)
+			if err != nil {
+				return fmt.Errorf("failed to get gas prices: %w", err)
+			}
+		}
+
+		logger.Debug("fetched gas price", "maxFeePerGas", maxFeePerGas, "maxPriorityFeePerGas", maxPriorityFeePerGas)
+
+		maxFeePerGas.Mul(maxFeePerGas, gasConfig.MaxFeePerGasMultiplier.BigInt())
+
+		maxPriorityFeePerGas.Mul(
+			maxPriorityFeePerGas,
+			gasConfig.MaxPriorityFeePerGasMultiplier.BigInt())
 
 		logger.Debug("calculated gas price", "maxFeePerGas", maxFeePerGas, "maxPriorityFeePerGas", maxPriorityFeePerGas)
 
-		op.MaxFeePerGas = decimal.NewFromBigInt(maxFeePerGas, 0)
-		op.MaxPriorityFeePerGas = decimal.NewFromBigInt(maxPriorityFeePerGas, 0)
+		if op.MaxFeePerGas.IsZero() {
+			op.MaxFeePerGas = decimal.NewFromBigInt(maxFeePerGas, 0)
+		}
+		if op.MaxPriorityFeePerGas.IsZero() {
+			op.MaxPriorityFeePerGas = decimal.NewFromBigInt(maxPriorityFeePerGas, 0)
+		}
 
 		return nil
 	}
-}
-
-// overwriteGasLimitsIfUnset applies gas limits if they are not already set.
-func overwriteGasLimitsIfUnset(
-	est GasEstimate,
-	op *UserOperation,
-) error {
-	preVerificationGas, verificationGasLimit, callGasLimit, err := est.convert()
-	if err != nil {
-		return fmt.Errorf("failed to convert gas estimates: %w", err)
-	}
-
-	if !op.CallGasLimit.IsZero() {
-		callGasLimit = op.CallGasLimit.BigInt()
-	}
-	if !op.VerificationGasLimit.IsZero() {
-		verificationGasLimit = op.VerificationGasLimit.BigInt()
-	}
-	if !op.PreVerificationGas.IsZero() {
-		preVerificationGas = op.PreVerificationGas.BigInt()
-	}
-
-	logger.Debug("estimated userOp gas", "callGasLimit", callGasLimit, "verificationGasLimit", verificationGasLimit, "preVerificationGas", preVerificationGas)
-
-	op.CallGasLimit = decimal.NewFromBigInt(callGasLimit, 0)
-	op.VerificationGasLimit = decimal.NewFromBigInt(verificationGasLimit, 0)
-	op.PreVerificationGas = decimal.NewFromBigInt(preVerificationGas, 0)
-
-	return nil
-}
-
-// overwriteGasLimits overwrites gas limits with the ones from the estimate.
-func overwriteGasLimits(
-	est GasEstimate,
-	op *UserOperation,
-) error {
-	preVerificationGas, verificationGasLimit, callGasLimit, err := est.convert()
-	if err != nil {
-		return fmt.Errorf("failed to convert gas estimates: %w", err)
-	}
-
-	logger.Debug("estimated userOp gas", "callGasLimit", callGasLimit, "verificationGasLimit", verificationGasLimit, "preVerificationGas", preVerificationGas)
-
-	op.CallGasLimit = decimal.NewFromBigInt(callGasLimit, 0)
-	op.VerificationGasLimit = decimal.NewFromBigInt(verificationGasLimit, 0)
-	op.PreVerificationGas = decimal.NewFromBigInt(preVerificationGas, 0)
-
-	return nil
 }
 
 func getBiconomyPaymasterAndData(
@@ -377,65 +325,6 @@ func getPimlicoVerifyingPaymasterAndData(
 		}
 
 		return overwriteGasLimits(gasEst, op)
-	}
-}
-
-// GasEstimate holds gas estimates for a user operation.
-type GasEstimate struct {
-	// depending on provider, any of the following types can be received here: string, int
-	CallGasLimit         any `json:"callGasLimit"`
-	VerificationGasLimit any `json:"verificationGasLimit"`
-	PreVerificationGas   any `json:"preVerificationGas"`
-
-	PaymasterAndData string `json:"paymasterAndData,omitempty"`
-}
-
-func (est GasEstimate) convert() (
-	preVerificationGas *big.Int,
-	verificationGasLimit *big.Int,
-	callGasLimit *big.Int,
-	err error,
-) {
-	preVerificationGas, err = est.fromAny(est.PreVerificationGas)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("preVerificationGas: %w", err)
-	}
-
-	verificationGasLimit, err = est.fromAny(est.VerificationGasLimit)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("verificationGasLimit: %w", err)
-	}
-
-	callGasLimit, err = est.fromAny(est.CallGasLimit)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("callGasLimit: %w", err)
-	}
-
-	return preVerificationGas,
-		verificationGasLimit,
-		callGasLimit,
-		nil
-}
-
-func (est GasEstimate) fromAny(a any) (*big.Int, error) {
-	switch v := a.(type) {
-	case string:
-		n, err := strconv.ParseInt(v, 16, 64)
-		if err != nil { // it IS hexadecimal
-			nBig, err := hexutil.DecodeBig(v)
-			if err != nil {
-				return nil, fmt.Errorf("failed to parse: %w (got '%s')", err, v)
-			}
-			return nBig, nil
-		}
-
-		return new(big.Int).SetInt64(n), nil
-	case int64:
-		return new(big.Int).SetInt64(v), nil
-	case float64:
-		return new(big.Int).SetInt64(int64(v)), nil
-	default:
-		return nil, fmt.Errorf("unexpected type: %T", v)
 	}
 }
 
