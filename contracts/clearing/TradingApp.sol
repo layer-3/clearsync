641612e89775e237284dcbf2c5d2226852512172
nksazonov <nsazonov@openware.com>
2024-11-26 18:01:01 +0200

    feat(trading app): update structs, implement tradingApp

--- contracts/clearing/TradingApp.sol
+++ contracts/clearing/TradingApp.sol
@@ -1,76 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-import '../nitro/interfaces/IForceMoveApp.sol';
-import '../nitro/libraries/NitroUtils.sol';
-import '../nitro/interfaces/INitroTypes.sol';
 import {ExitFormat as Outcome} from '@statechannels/exit-format/contracts/ExitFormat.sol';
 
-/*
-    - both Order and Trade contain sigs of user and broker respectively
-    - Order and Trade are coupled by some id (that may be calculated based on the order itself)
-    - State invariant: for each Order there is a respective Trade, and both are valid (not 0 amounts, etc)
-    - State transition rules:
-        - amount of Order-Trade pairs can increase without restriction
-        - amount of Order-Trade pairs can decrease only after the settlement state
-    - settlement state is a state with `settlementData` not
-    - to settle using the TradingVault, settlement state must be signed by the Broker and *maybe* by the user
- */
+import {StrictTurnTaking} from '../nitro/libraries/signature-logic/StrictTurnTaking.sol';
+import {Consensus} from '../nitro/libraries/signature-logic/Consensus.sol';
+import {IForceMoveApp} from '../nitro/interfaces/IForceMoveApp.sol';
+import {NitroUtils} from '../nitro/libraries/NitroUtils.sol';
+import {INitroTypes} from '../nitro/interfaces/INitroTypes.sol';
 
-// TODO: optimize for storage slots
-interface TradingStructs {
-	enum Side {
-		BUY,
-		SELL
-	}
-
+interface ITradingStructs {
 	struct Order {
-		Side side;
-		address token;
+		bytes32 orderID; // tradeID
+	}
+
+	enum OrderResponseType {
+		ACCEPT,
+		REJECT
+	}
+
+	struct OrderResponse {
+		OrderResponseType responseType;
+		bytes32 orderID; // orderID making the trade
+	}
+
+	struct AssetAndAmount {
+		address asset;
 		uint256 amount;
-		uint256 ts; // timestamp in seconds
-		bytes signature; // user signature
-	}
-
-	// NOTE: there is no need in `tradeId` as it is not checked in any way.
-	// If you want to signal Trade settlement, then use settlementId in the event instead
-	struct Trade {
-		bytes32 orderId; // keccak256(abi.encode(Order))
-		uint256 amount; // amount executed
-		uint256 ts; // timestamp in seconds
-		bytes signature; // broker signature
-	}
-
-	enum FundingLocation {
-		/// @dev funds are taken from the TradingVault account's balance
-		TradingVault,
-		/// @dev funds are pulled from the account's token balance
-		Address
-	}
-
-	struct CounterpartiesFundingLocations {
-		FundingLocation[] user;
-		FundingLocation[] broker;
-	}
-
-	struct OrderTradePair {
-		Order order;
-		Trade trade;
 	}
 
 	struct Settlement {
-		bytes32 id; // keccak256(abi.encode(OrderTradePair[]));
-		CounterpartiesFundingLocations sourceFLs;
-		CounterpartiesFundingLocations destinationFLs;
-	}
-
-	// is encoded into the `state` field of the variablePart
-	struct State {
-		OrderTradePair[] pairs;
-		bytes settlementData; // 0x if state is not settlement, encoded `Settlement` if it is
+		AssetAndAmount[] toTrader;
+		AssetAndAmount[] toBroker;
 	}
 }
 
+// FIXME: should Vault support multiple brokers?
 interface ISettle {
 	function settle(
 		INitroTypes.FixedPart calldata fixedPart,
@@ -84,5 +49,89 @@ contract TradingApp is IForceMoveApp {
 		FixedPart calldata fixedPart,
 		RecoveredVariablePart[] calldata proof,
 		RecoveredVariablePart calldata candidate
-	) external pure override returns (bool, string memory) {}
+	) external pure override returns (bool, string memory) {
+		// FIXME: does the Broker deposit to the Adjudicator?
+		// turn nums:
+		// 0 - prefund
+		// 1 - postfund
+		// 2 - order
+		// 2n+1 - order response
+		// 2n - order or settlement
+
+		uint48 candTurnNum = candidate.variablePart.turnNum;
+
+		// prefund or postfund
+		if (candTurnNum == 0 || candTurnNum == 1) {
+			Consensus.requireConsensus(fixedPart, proof, candidate);
+			return (true, '');
+		}
+
+		bytes memory candidateData = candidate.variablePart.appData;
+		// settlement
+		// TODO: unsure whether we should check the proof when consensus is reached
+		if (candTurnNum % 2 == 0 && proof.length == 0) {
+			Consensus.requireConsensus(fixedPart, proof, candidate);
+			// NOTE: used just to check the data structure validity
+			ITradingStructs.Settlement memory _unused = abi.decode(
+				candidateData,
+				(ITradingStructs.Settlement)
+			);
+			return (true, '');
+		}
+
+		// participant 0 signs even turns
+		// participant 1 signs odd turns
+		StrictTurnTaking.requireValidTurnTaking(fixedPart, proof, candidate);
+		require(proof.length == 2, 'proof.length < 2');
+		(VariablePart memory proof0, VariablePart memory proof1) = (
+			proof[0].variablePart,
+			proof[1].variablePart
+		);
+		require(proof0.turnNum == candTurnNum - 2, 'proof0.turnNum != candTurnNum - 1');
+		require(proof1.turnNum == candTurnNum - 1, 'proof1.turnNum != candTurnNum - 1');
+
+		// order
+		if (candTurnNum % 2 == 0) {
+			ITradingStructs.Order memory prevOrder = abi.decode(
+				proof0.appData,
+				(ITradingStructs.Order)
+			);
+			ITradingStructs.OrderResponse memory prevOrderResponse = abi.decode(
+				proof1.appData,
+				(ITradingStructs.OrderResponse)
+			);
+			if (prevOrderResponse.responseType == ITradingStructs.OrderResponseType.ACCEPT) {
+				require(
+					prevOrderResponse.orderID == prevOrder.orderID,
+					'orderResponse.orderID != prevOrder.orderID, candidate is order'
+				);
+			}
+			// NOTE: used just to check the data structure validity
+			ITradingStructs.Order memory _candOrder = abi.decode(
+				candidateData,
+				(ITradingStructs.Order)
+			);
+			return (true, '');
+		}
+
+		// orderResponse
+		// NOTE: used just to check the data structure validity
+		ITradingStructs.OrderResponse memory _prevOrderResponse = abi.decode(
+			proof0.appData,
+			(ITradingStructs.OrderResponse)
+		);
+
+		ITradingStructs.Order memory order = abi.decode(proof1.appData, (ITradingStructs.Order));
+		ITradingStructs.OrderResponse memory orderResponse = abi.decode(
+			candidateData,
+			(ITradingStructs.OrderResponse)
+		);
+		if (orderResponse.responseType == ITradingStructs.OrderResponseType.ACCEPT) {
+			require(
+				orderResponse.orderID == order.orderID,
+				'orderResponse.orderID != order.orderID, candidate is orderResponse'
+			);
+		}
+		return (true, '');
+	}
 }
